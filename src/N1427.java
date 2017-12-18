22b14230b3d4fb1e5df41c594e445bc4fca45ff5
JSoi <jsoi950715@gmail.com>
2017-12-18 21:48:18 +0900

    #1427

---
+++ src/N1427.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N1427 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		scan.close();
+		char charValue[] = input.toCharArray();
+		Arrays.sort(charValue);
+		for(int i = charValue.length-1; i >=0; i--) {
+			System.out.print(charValue[i]);
+		}
+	}
+}
