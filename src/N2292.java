51e1f0ef027485fe9cb80875cf98a9503a7367cd
JSoi <jsoi950715@gmail.com>
2018-06-30 15:03:14 +0900

    Baekjoon Algortihm Problem 2292

---
+++ src/N2292.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class N2292 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long userInteger = scan.nextInt();
+		scan.close();
+		long lastNum = 1;
+		long timeCal = 0;
+		while (true) {
+			if (lastNum <= userInteger && userInteger <= lastNum + timeCal * 6) {
+				break;
+			}
+			lastNum += timeCal * 6;
+			timeCal++;
+		}
+		System.out.println(timeCal+1);
+	}
+
+}
