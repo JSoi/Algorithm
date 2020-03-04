b16a9bcdcb8f969e8348697b81307be1b5aa7793
JSOI <JSOI@DESKTOP-3ENE0KU>
2020-03-04 19:55:39 +0900

    프로그래머스 문제 추가

--- src/programmers/L17682.java
+++ src/programmers/L17682.java
@@ -4,31 +4,39 @@ public class L17682 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		solution("1S2D*3T");
+		System.out.println(solution("1S2D*3T"));
+		System.out.println(solution("1D2S#10S"));
 	}
 
 	public static int solution(String dartResult) {
-		int numC = 0;
-		int totalNum = dartResult.split("S|D|T").length;
-		int[] resultArr = new int[totalNum];
 		String temp = "";
+		int ints[] = new int[3];
+		int intscount = 0;
 		for (int i = 0; i < dartResult.length(); i++) {
-			char now = dartResult.charAt(i);
-			if (now == 'S') {
-				
-			} else if (now == 'D') {
-
-			} else if (now == 'T') {
-
-			} else if (now == '#') {
-
-			} else if (now == '*') {
-
-			} else {
-				temp += dartResult.charAt(i);
+			char test = dartResult.charAt(i);
+			if (test == 'S' || test == 'D' || test == 'T') {
+				int val = Integer.valueOf(temp);
+				if (test == 'D')
+					val *= val;
+				else if (test == 'T')
+					val *= val * val;
+				ints[intscount++] = val;
+				temp = "";
+			} else if (test >= '0' && test <= '9') {
+				temp += test; // 10일 경우 대비하여
+			} else {// 옵션
+				int target = intscount - 1; // 다음 index를 가르키고 있으므로 현재 인덱스로 반환해준다
+				if (test == '*') {
+					for (int j = target; j >= target - 1; j--) {
+						if(j < 0) break;
+						ints[j] *= 2;
+					}
+				} else {
+					ints[target] *= -1;
+				}
 			}
 		}
-		int answer = 0;
+		int answer = ints[0] + ints[1] + ints[2];
 		return answer;
 	}
 }
