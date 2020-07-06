ac37c039c3fc12f6b1f52cd18704bc84a9844fab
JSOI <JSOI@DESKTOP-3ENE0KU>
2020-07-06 17:50:04 +0900

    Programmer 42578, 42747 solved

--- src/programmers/L42747.java
+++ src/programmers/L42747.java
@@ -7,18 +7,22 @@ public class L42747 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// System.out.println(solution(new int[] { 3, 0, 6, 1, 5 }));
-		System.out.println(solution(new int[] { 5, 5, 5, 5}));
+		System.out.println(solution(new int[] { 5, 5, 5, 5 }));
+		// System.out.println(solution(new int[] { 22, 42 }));
 	}
 
 	public static int solution(int[] citations) {
-		int answer = 0;
 		Arrays.sort(citations);
+		int answer = 0;
 		for (int i = citations[0]; i <= citations[citations.length - 1]; i++) {
 			// 기준 값을 가지고 바로 큰 값이 나오게 되면.... 종료하고 값을 계산해 리턴한다
 			int compare = i;
 			int down = 0;
 			int same = 0;
 			int up = 0;
+			if (citations[0] >= citations.length)
+				// 만약 가장 작은 원소가 배열의 크기보다 작게 된다면, 배열의 길이가 정답이 된다.
+				return citations.length;
 			for (int j = 0; j < citations.length; j++) {
 				if (citations[j] > compare) {
 					break;
@@ -31,9 +35,7 @@ public class L42747 {
 			down += same;
 			up = citations.length - down + same;
 			System.out.println("down  : " + down + " / up : " + up);
-			System.out.println("compare : " + compare);
-			
-			if (up >= compare && citations.length-up <= compare)
+			if (up >= compare && down <= compare)
 				answer = Math.max(answer, compare);
 		}
 		return answer;
