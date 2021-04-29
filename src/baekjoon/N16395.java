a1b794e5441f11ea4b9a2ca709636812b4a13c35
JSOI <JSOI@DESKTOP-3ENE0KU>
2021-04-29 16:38:00 +0900

    BOJ 9655, 16395 Solved(DP)

---
+++ src/baekjoon/N16395.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class N16395 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		scan.close();
+		int[][] p = new int[n][n];
+		// initialize
+		for (int i = 0; i < n; i++) {
+			p[i][0] = 1;
+			p[0][i] = 1;
+		}
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n - i; j++) {
+				p[i][j] = p[i][j - 1] + p[i - 1][j];
+			}
+		}
+		System.out.println(p[n-k][k-1]);
+	}
+
+}
---
+++ src/programmers/L49994.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class L49994 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+		sol.solution("ULURRDLLU");
+	}
+
+	private static class Solution {
+		public int solution(String dirs) {
+			int answer = 0;
+			char[] dir = dirs.toCharArray();
+			return answer;
+		}
+	}
+
+	private static class Dot {
+		int x, y;
+
+		Dot(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
