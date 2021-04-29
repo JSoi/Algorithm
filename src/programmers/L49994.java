6530dbb1a38fa136948d3a2612864b048d55626c
JSOI <JSOI@DESKTOP-3ENE0KU>
2021-04-29 18:17:20 +0900

    PGMRS 49994(DP)

--- src/programmers/L49994.java
+++ src/programmers/L49994.java
@@ -1,27 +1,55 @@
 package programmers;
 
+import java.util.HashSet;
+
 public class L49994 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution sol = new Solution();
-		sol.solution("ULURRDLLU");
+		System.out.println(sol.solution("LULLLLLLU"));
 	}
 
-	private static class Solution {
+	public static class Solution {
 		public int solution(String dirs) {
 			int answer = 0;
-			char[] dir = dirs.toCharArray();
+			int dx[] = new int[] { 0, 0, -1, 1 };
+			int dy[] = new int[] { 1, -1, 0, 0 };
+			boolean visit[][][][] = new boolean[11][11][11][11];
+			visit[5][5][5][5] = true;
+			int startX = 5;
+			int startY = 5;
+			char[] arr = dirs.toCharArray();
+			for (char a : arr) {
+				int nextX = startX;
+				int nextY = startY;
+				int dIndex = -1;
+				if (a == 'U') {
+					dIndex = 0;
+				} else if (a == 'D') {
+					dIndex = 1;
+				} else if (a == 'L') {
+					dIndex = 2;
+				} else {
+					dIndex = 3;
+				}
+
+				nextX = startX + dx[dIndex];
+				nextY = startY + dy[dIndex];
+				// 이전에 움직인 범위에 의해 캐릭터의 위치가 지도를 벗어났을 경우
+				if (nextX < 0 || nextY < 0 || nextX > 10 || nextY > 10) {
+					// 다시 캐릭터를 전의 위치로 이동
+					continue;
+				}
+				if (!visit[startX][startY][nextX][nextY] && !visit[nextX][nextY][startX][startY]) {
+					visit[startX][startY][nextX][nextY] = true;
+					visit[nextX][nextY][startX][startY] = true;
+					answer++;
+				}
+				startX = nextX;
+				startY = nextY;
+			}
 			return answer;
-		}
-	}
-
-	private static class Dot {
-		int x, y;
-
-		Dot(int x, int y) {
-			this.x = x;
-			this.y = y;
 		}
 	}
 }
