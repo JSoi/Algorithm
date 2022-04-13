7d600ae6d1cdf1e75d285451a7317ca0159414df
JSOI <jsoi95071@gmail.com>
2022-04-13 13:09:25 +0900

    two pointer

---
+++ src/baekjoon/N1253.java
@@ -0,0 +1,45 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N1253 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = Integer.parseInt(buf.readLine());
+		long num[] = new long[cnt];
+		int answer = 0;
+		StringTokenizer tok = new StringTokenizer(buf.readLine());
+		for (int i = 0; i < cnt; i++) {
+			num[i] = Long.parseLong(tok.nextToken());
+		}
+		Arrays.sort(num);
+		for (int i = 0; i < cnt; i++) {
+			long find = num[i];
+			int s = 0, e = cnt - 1;
+			while (s < e) {
+				if (num[s] + num[e] == find) {
+					if (s != i && e != i) {
+						answer++;
+						break;
+					} else if (s == i) {
+						s++;
+					} else if (e == i) {
+						e--;
+					}
+				} else if (num[s] + num[e] < find) {
+					s++;
+				} else {
+					e--;
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
