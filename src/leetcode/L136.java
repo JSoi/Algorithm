b1d2eac369d1c2e2de31c808721cfaf9e5dae4a8
JSOI <jsoi950715@gmail.com>
2022-05-09 18:58:49 +0900

    Leetcode Day14 Bit Manipulation 136

---
+++ src/leetcode/L136.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class L136 {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int num = 0;
+        int none = -1;
+        boolean[] visit = new boolean[nums.length];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (visit[i]) continue;
+                if ((nums[i] ^ nums[j]) == 0) {
+                    visit[i] = true;
+                    visit[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < visit.length; i++) {
+            if (!visit[i]) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
