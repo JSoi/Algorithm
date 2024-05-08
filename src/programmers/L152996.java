4ecf7a076d88b16fb0924e457971401a4dcb7c7d
JSoi <jsoi950715@gmail.com>
2024-05-08 14:45:59 +0900

    PG L152996 Solving TO - Set

--- src/programmers/L152996.java
+++ src/programmers/L152996.java
@@ -1,33 +1,43 @@
 package programmers;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class L152996 {
+    public static final int[] dist = {2, 3, 4};
+
     public long solution(int[] weights) {
-        long answer = 0;
+        int answer = 0;
         Arrays.sort(weights);
-        int[][] array = Arrays.stream(weights)
-                .mapToObj(weight -> new int[]{weight * 2, weight * 3, weight * 4})
-                .toArray(int[][]::new);
-        Set<Integer> seen = new HashSet<>();
+        Map<Integer, Set<Integer>> seenCount = new HashMap<>();
+        int index = 0;
+        for (int weight : weights) {
+            for (int d : dist) {
+                seenCount.computeIfAbsent(index, k -> new HashSet<>()).add(weight * d);
+            }
+            index++;
+        }
         for (int i = 0; i < weights.length; i++) {
             for (int j = i + 1; j < weights.length; j++) {
-                seen.clear();
-                seen.addAll(Arrays.asList(array[i][0], array[i][1], array[i][2]));
-                for (int k = 0; k < 3; k++) {
-                    if (seen.contains(array[j][k])) {
-                        answer++;
-                        break;
-                    }
+                if (contains(seenCount.get(i), seenCount.get(j))) {
+                    answer++;
                 }
             }
         }
         return answer;
     }
+
+    public boolean contains(Set<Integer> set1, Set<Integer> set2) {
+        for (int s1 : set1) {
+            if (set2.contains(s1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         L152996 solution = new L152996();
-        System.out.println(solution.solution(new int[]{100,180,360,100,270}));
+        System.out.println(solution.solution(new int[]{100, 180, 360, 100, 270}));
     }
 }
