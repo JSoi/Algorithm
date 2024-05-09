65fb84a9a8724ead265484853c30c22025216f18
JSoi <jsoi950715@gmail.com>
2024-05-09 11:36:07 +0900

    PG L152996 Solving TO - Set V2

--- src/programmers/L152996.java
+++ src/programmers/L152996.java
@@ -1,43 +1,38 @@
 package programmers;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class L152996 {
     public static final int[] dist = {2, 3, 4};
+    // weight, index
+    public static Map<Integer, Set<Integer>> setMap = new HashMap<>();
 
-    public long solution(int[] weights) {
+    public static long solution(int[] weights) {
         int answer = 0;
         Arrays.sort(weights);
-        Map<Integer, Set<Integer>> seenCount = new HashMap<>();
-        int index = 0;
-        for (int weight : weights) {
-            for (int d : dist) {
-                seenCount.computeIfAbsent(index, k -> new HashSet<>()).add(weight * d);
-            }
-            index++;
-        }
-        for (int i = 0; i < weights.length; i++) {
-            for (int j = i + 1; j < weights.length; j++) {
-                if (contains(seenCount.get(i), seenCount.get(j))) {
-                    answer++;
-                }
+        for (int k = 0; k < weights.length; k++) {
+            answer += getElement(weights[k], k);
+            for(int d : dist){
+                setMap.computeIfAbsent(weights[k] * d, key -> new HashSet<>()).add(k);
             }
         }
         return answer;
     }
 
-    public boolean contains(Set<Integer> set1, Set<Integer> set2) {
-        for (int s1 : set1) {
-            if (set2.contains(s1)) {
-                return true;
-            }
+    public static int getElement(int original, int index) {
+        Set<Integer> elements = new HashSet<>();
+        for(int d : dist){
+            elements.addAll(setMap.getOrDefault(original * d, Set.of()));
         }
-        return false;
+        elements.remove(index);
+        return elements.size();
     }
 
     public static void main(String[] args) {
         L152996 solution = new L152996();
+        L152996 solution2 = new L152996();
         System.out.println(solution.solution(new int[]{100, 180, 360, 100, 270}));
+//        System.out.println("L152996.main");
+//        System.out.println(solution2.solution(new int[]{100, 100, 100, 100, 100}));
     }
 }
