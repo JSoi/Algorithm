6ae4000954742c83dcf650915122df891db6e2e7
JSoi <jsoi950715@gmail.com>
2024-05-14 12:12:15 +0900

    PG L176963 Solved

---
+++ src/programmers/L176963.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/176963">추억 점수</a>
+ */
+public class L176963 {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> collect =
+                IntStream.range(0, name.length).boxed()
+                        .collect(Collectors.toMap(key -> name[key], value -> yearning[value], (a, b) -> a, HashMap::new));
+        return Arrays.stream(photo)
+                .mapToInt(p -> Arrays.stream(p).mapToInt(s -> collect.getOrDefault(s, 0)).sum()).toArray();
+    }
+
+    public static void main(String[] args) {
+        L176963 l176963 = new L176963();
+        int[] solution = l176963.solution(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}});
+//        System.out.println(("[[\"may\", \"kein\", \"kain\", \"radi\"],[\"may\", \"kein\", \"brin\", \"deny\"], [\"kon\", \"kain\", \"may\", \"coni\"]]")
+//                .replaceAll("\\[", "\\{").replaceAll("\\]", "\\}"));
+        System.out.println(Arrays.toString(solution));
+
+    }
+}
