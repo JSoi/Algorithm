9039fcb3dcb1b44f4fb4c424b9fd7fadc68187f9
JSoi <jsoi950715@gmail.com>
2024-07-05 23:57:58 +0900

    PG L169198 fixed

--- src/programmers/L169198.java
+++ src/programmers/L169198.java
@@ -23,19 +23,45 @@ public class L169198 {
 
     private int between(int x1, int y1, int x2, int y2, int minX, int minY) {
         int yAnswer, xAnswer;
-        // x축에 평항
+        // x축에 평행
         xAnswer = (int) Math.pow(Math.abs(x1 - x2), 2);
-        xAnswer += (int) Math.pow(Math.min(y1 + y2, 2 * minY - (y1 + y2)), 2);
-        // y축 평행
+        // 윗쪽
+        int xTemp = getxTemp(x1, y1, x2, y2, minY);
+        xAnswer += xTemp;
+        // 아래쪽
         yAnswer = (int) Math.pow(Math.abs(y1 - y2), 2);
-        yAnswer += (int) Math.pow(Math.min(x1 + x2, 2 * minX - (x1 + x2)), 2);
-        if (x1 == x2) {
-            return yAnswer;
-        }
-        if (y1 == y2) {
-            return xAnswer;
-        }
-        System.out.println(xAnswer + " " + yAnswer);
+        // y축 평행
+        int yTemp = getyTemp(x1, y1, x2, y2, minX);
+        System.out.println(yTemp + "]]");
+        yAnswer += yTemp;
+        System.out.println(" --- --- " + xAnswer + " " + yAnswer);
         return Math.min(xAnswer, yAnswer);
+    }
+
+    private static int getxTemp(int x1, int y1, int x2, int y2, int minY) {
+        int xTemp = Integer.MAX_VALUE;
+        if (x1 == x2) { // 가로막힐 경우
+            if (y1 < y2) {
+                xTemp = (int) Math.pow(y1 + y2, 2);
+
+            } else {
+                xTemp = (int) Math.pow(2 * minY - (y1 + y2), 2);
+            }
+            return xTemp;
+        }
+        return Math.min(xTemp, (int) Math.pow(Math.min(y1 + y2, 2 * minY - (y1 + y2)), 2));
+    }
+
+    private static int getyTemp(int x1, int y1, int x2, int y2, int minX) {
+        int yTemp = Integer.MAX_VALUE;
+        if (y1 == y2) { // 가로막힐 경우
+            if (x1 < x2) {
+                yTemp = (int) Math.pow(x1 + x2, 2);
+            } else {
+                yTemp = (int) Math.pow(2 * minX - (x1 + x2), 2);
+            }
+            return yTemp;
+        }
+        return Math.min(yTemp, (int) Math.pow(Math.min(x1 + x2, 2 * minX - (x1 + x2)), 2));
     }
 }
