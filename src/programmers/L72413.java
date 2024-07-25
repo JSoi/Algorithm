cc2dc47e29c4d653f3dbeee0039adf0b6a45ad6b
JSoi <jsoi950715@gmail.com>
2024-07-26 01:05:13 +0900

    PG L72413 Fixed

--- src/programmers/L72413.java
+++ src/programmers/L72413.java
@@ -1,5 +1,9 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class L72413 {
     public static void main(String[] args) {
         int solution = new L72413().solution(6, 4, 6, 2, new int[][]{{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}});
@@ -7,37 +11,37 @@ public class L72413 {
 
     }
 
-    static int[][] roads;
-    static int answer = Integer.MAX_VALUE;
-    static int aDestination;
-    static int bDestination;
 
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        roads = new int[n][n];
-        for (int[] fare : fares) {
-            roads[fare[0] - 1][fare[1] - 1] = roads[fare[1] - 1][fare[0] - 1] = fare[2];
+        int[][] distances = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(distances[i], Integer.MAX_VALUE);
         }
-        aDestination = a - 1;
-        bDestination = b - 1;
-        boolean[] visit = new boolean[n];
-        visit[s - 1] = true;
-        ride(0, s - 1, visit);
-        return answer;
-    }
 
-    private static void ride(int totalFee, int visitSpot, boolean[] visit) {
-        if (visit[aDestination] && visit[bDestination]) {
-//            System.out.println(" = " + totalFee + "\n");
-            answer = Math.min(answer, totalFee);
-            return;
+        for (int[] fare : fares) {
+            distances[fare[0] - 1][fare[1] - 1] = distances[fare[1] - 1][fare[0] - 1] = fare[2];
         }
-        for (int i = 0; i < roads.length; i++) {
-            if (roads[visitSpot][i] != 0 && !visit[i]) {
-                visit[i] = true;
-//                System.out.printf((visitSpot+1) + " -> " + (i+1) + " ");
-                ride(totalFee + roads[visitSpot][i], i, visit);
-                visit[i] = false;
+
+        // Floyd-Warshall algorithm
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (distances[i][k] == Integer.MAX_VALUE || distances[k][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    distances[i][j] = i == j ? 0 : Math.min(distances[i][j], distances[i][k] + distances[k][j]);
+                }
             }
         }
+
+        // Calculate minimum fare
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (distances[s - 1][i] == Integer.MAX_VALUE || distances[i][a - 1] == Integer.MAX_VALUE || distances[i][b - 1] == Integer.MAX_VALUE) {
+                continue;
+            }
+            answer = Math.min(answer, distances[s - 1][i] + distances[i][a - 1] + distances[i][b - 1]);
+        }
+        return answer;
     }
 }
