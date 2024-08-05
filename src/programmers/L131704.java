aac4aaead61ec45693248ec6639390f61fe78a0c
JSoi <jsoi950715@gmail.com>
2024-08-05 12:31:04 +0900

    PG L131704 Fixed

--- src/programmers/L131704.java
+++ src/programmers/L131704.java
@@ -1,38 +1,28 @@
 package programmers;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import tool.Assertions;
+
 import java.util.Stack;
-import java.util.stream.Collectors;
 
 public class L131704 {
-    public static void main(String[] args) {
-        System.out.println(new L131704().solution(new int[]{4, 3, 1, 2, 5}));
-        System.out.println(new L131704().solution(new int[]{5, 4, 3, 2, 1}));
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        Assertions.check(new L131704().solution(new int[]{4, 3, 1, 2, 5}), 2);
+        Assertions.check(new L131704().solution(new int[]{5, 4, 3, 2, 1}), 5);
+        Assertions.check(new L131704().solution(new int[]{6, 7, 5, 4, 3, 2, 1}), 7);
+
     }
 
     public int solution(int[] order) {
-        Queue<Integer> queue = new LinkedList<>(Arrays.stream(order).boxed().collect(Collectors.toList()));
-        Stack<Integer> waitStack = new Stack<>();
-        int count = 1;
-        while (count < order.length+1) {
-            if (queue.isEmpty() && waitStack.peek() != count) {
-                break;
-            }
-            if (!queue.isEmpty()) {
-                if (count != queue.peek()) {
-                    waitStack.push(queue.poll());
-                } else {
-                    queue.poll();
-                    count++;
-                }
-            } else if (!waitStack.isEmpty() && count == waitStack.peek()) {
-                waitStack.pop();
-                count++;
+        Stack<Integer> stack = new Stack<>();
+        int orderIndex = 0;
+        for (int count = 1; count <= order.length; count++) {
+            stack.push(count);
+            while (!stack.isEmpty() && stack.peek() == order[orderIndex]) {
+                orderIndex++;
+                stack.pop();
             }
         }
-        return count;
-    // 4 3 1 2 5
-}
+        return orderIndex;
+    }
 }
