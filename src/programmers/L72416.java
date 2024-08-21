a799df7bf36cfdebd3a08dd82c592c6f2c84905d
JSoi <jsoi950715@gmail.com>
2024-08-21 23:53:16 +0900

    PG 72416 TO Fixed

--- src/programmers/L72416.java
+++ src/programmers/L72416.java
@@ -12,22 +12,20 @@ public class L72416 {
 //        System.out.println(solution2);
     }
 
-    //    static int[] tree;
-    static int[] leader;
     static int[] team;
-    static int[][] cost;
     static int[] loss;
-    static boolean[] visit;
+    static int[][] cost;
+    static Map<Integer, Set<Integer>> teamMap;
 
     public static int solution(int[] sales, int[][] links) {
         loss = sales;
         cost = new int[sales.length][2];
-        visit = new boolean[sales.length];
-        leader = IntStream.range(0, sales.length).map(i -> i == 0 ? 0 : -1).toArray();
+        teamMap = new HashMap<>();
         team = new int[sales.length];
         for (int[] link : links) {
-            leader[link[0] - 1] = link[0] - 1;
             team[link[1] - 1] = link[0] - 1;
+            teamMap.putIfAbsent(link[0] - 1, new HashSet<>());
+            teamMap.get(link[0] - 1).add(link[1] - 1);
         }
         dfs(0);
         return Math.min(cost[0][0], cost[0][1]);
@@ -36,10 +34,9 @@ public class L72416 {
     private static void dfs(int start) {
         Stack<Integer> stack = new Stack<>();
         stack.push(start);
-        visit[start] = true;
         while (!stack.isEmpty()) {
             int current = stack.pop();
-            if (leader[current] == -1) { // Leaf node
+            if (!teamMap.containsKey(current)) { // Leaf node
                 cost[current][0] = 0;
                 cost[current][1] = loss[current];
                 continue;
@@ -48,15 +45,15 @@ public class L72416 {
             int withoutLeader = 0;
             boolean childParticipate = false;
             int minDiff = Integer.MAX_VALUE;
-            for (int i = 0; i < leader.length; i++) {
-                if (team[i] == current && !visit[i]) {
-                    dfs(i);
-                    withLeader += Math.min(cost[i][0], cost[i][1]);
-                    withoutLeader += Math.min(cost[i][0], cost[i][1]);
-                    if (cost[i][0] >= cost[i][1]) {
+            for (int child : teamMap.get(start)) {
+                if (team[child] == current) {
+                    dfs(child);
+                    withLeader += Math.min(cost[child][0], cost[child][1]);
+                    withoutLeader += Math.min(cost[child][0], cost[child][1]);
+                    if (cost[child][0] >= cost[child][1]) {
                         childParticipate = true;
                     }
-                    minDiff = Math.min(minDiff, cost[i][1] - cost[i][0]);
+                    minDiff = Math.min(minDiff, cost[child][1] - cost[child][0]);
                 }
             }
             withoutLeader += childParticipate ? 0 : minDiff;
