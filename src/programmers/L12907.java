1c0bf7fddb3c3ba0b0aa39d344c7805fae7b4ae7
JSoi <jsoi950715@gmail.com>
2024-08-22 14:41:14 +0900

    PG L12907 DP

---
+++ src/programmers/L12907.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class L12907 {
+    public static void main(String[] args) {
+        int solution = new L12907().solution(5, new int[]{1, 2, 5});
+        System.out.println(solution);
+    }
+
+
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+            }
+        }
+        return dp[n];
+    }
+}
