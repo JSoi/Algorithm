63f541fd7d1f54e0b9fff14cf111ad4ca04a9318
JSoi <jsoi950715@gmail.com>
2024-08-24 21:47:03 +0900

    PG L159993 Modified to Queue

--- src/programmers/L159993.java
+++ src/programmers/L159993.java
@@ -1,5 +1,9 @@
 package programmers;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/159993">미로 탈출</a>
  */
@@ -27,39 +31,66 @@ public class L159993 {
                 point[3] = new int[]{i, maps[i].indexOf("E")};
             }
         }
-        int toLeverAndFinal;
-        visited = new boolean[maps.length][maps[0].length()];
-        visited[point[0][0]][point[0][1]] = true;
-        travel(point[0][0], point[0][1], point[2][0], point[2][1], 0);
-        if (answer == -1) {
-            return answer;
+        int toLever = find(point[0][0], point[2][0], point[0][1], point[2][1]);
+        if (toLever == -1) {
+            return -1;
         }
-        toLeverAndFinal = answer;
-        answer = -1;
-        visited = new boolean[maps.length][maps[0].length()];
-        visited[point[2][0]][point[2][1]] = true;
-        travel(point[2][0], point[2][1], point[3][0], point[3][1], 0);
-        if (answer == -1) {
-            return answer;
+        int leverToDestination = find(point[2][0], point[3][0], point[2][1], point[3][1]);
+        if (leverToDestination == -1) {
+            return -1;
         }
-        toLeverAndFinal += answer;
-        return toLeverAndFinal;
+        return toLever + leverToDestination;
+
     }
 
-    public static void travel(int h, int v, int endH, int endV, int walkCount) {
-        if (h == endH && v == endV) {
+    static int find(int fromR, int toR, int fromC, int toC) {
+        boolean[][] visit = new boolean[mapChar.length][mapChar[0].length];
+        Queue<Node> queue = new LinkedList<>();
+        int answer = Integer.MAX_VALUE;
+        queue.offer(new Node(fromR, fromC, 0));
+        while (!queue.isEmpty()) {
+            Node target = queue.poll();
+            if (target.r == toR && target.c == toC) {
+                answer = Math.min(target.distance, answer);
+            }
+            for (int[] dir : directions) {
+                int nextR = target.r + dir[0];
+                int nextC = target.c + dir[1];
+                if (nextR < 0 || nextR >= mapChar.length || nextC < 0 || nextC >= mapChar[0].length || visit[nextR][nextC] || mapChar[nextR][nextC] == 'X') {
+                    continue;
+                }
+                visit[nextR][nextC] = true;
+                queue.offer(new Node(nextR, nextC, target.distance + 1));
+            }
+        }
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+
+    static class Node {
+        int r, c;
+        int distance;
+
+        public Node(int r, int c, int distance) {
+            this.r = r;
+            this.c = c;
+            this.distance = distance;
+        }
+    }
+
+    public static void travel(int r, int c, int endH, int endV, int walkCount) {
+        if (r == endH && c == endV) {
             answer = answer == -1 ? walkCount : Math.min(answer, walkCount);
             return;
         }
         for (int[] dir : directions) {
-            int nextH = h + dir[0];
-            int nextV = v + dir[1];
-            if (nextH < 0 || nextH >= mapChar.length || nextV < 0 || nextV >= mapChar[0].length || visited[nextH][nextV]|| mapChar[nextH][nextV] == 'X') {
+            int nextR = r + dir[0];
+            int nextC = c + dir[1];
+            if (nextR < 0 || nextR >= mapChar.length || nextC < 0 || nextC >= mapChar[0].length || visited[nextR][nextC] || mapChar[nextR][nextC] == 'X') {
                 continue;
             }
-            visited[nextH][nextV] = true;
-            travel(nextH, nextV, endH, endV, walkCount + 1);
-            visited[nextH][nextV] = false;
+            visited[nextR][nextC] = true;
+            travel(nextR, nextC, endH, endV, walkCount + 1);
+            visited[nextR][nextC] = false;
         }
     }
 
