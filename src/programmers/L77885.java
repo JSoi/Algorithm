88798094a2e5b0c40b860ce0aec5a945ba96499e
JSoi <jsoi950715@gmail.com>
2024-09-03 11:37:35 +0900

    POG L77885 TO Fixed

--- src/programmers/L77885.java
+++ src/programmers/L77885.java
@@ -5,34 +5,30 @@ import java.util.Arrays;
 public class L77885 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new L77885().solution(new long[]{2, 7})));
-
     }
 
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         int answerIndex = 0;
         for (long n : numbers) {
-            long another = n + 1;
-            while (true) {
-                if (differenceCount(n, another) <= 2) {
-                    answer[answerIndex++] = another;
-                    break;
-                }
-                another++;
-            }
+            long number = createNumber(n);
+            answer[answerIndex++] = number;
         }
         return answer;
     }
 
-    static int differenceCount(long a, long b) {
-        long xor = a ^ b;
-        int diffCount = 0;
-        while (xor > 0) {
-            if (xor % 2 == 1) {
-                diffCount++;
-            }
-            xor /= 2;
+    static long createNumber(long a) {
+        if ((a & 1L) == 0 || (a & 2L) == 0) {
+            return a + 1L;
         }
-        return diffCount;
+        long move = 1L;
+        while (true) {
+            if (((1L << move) & a) == 0) { // 00, 10
+                return a | (1L << (move));
+            } else if (((2L << move) & a) == 0) { // 01
+                return (a + 1) | (1L << (move)) - 1;
+            }
+            move++;
+        }
     }
 }
