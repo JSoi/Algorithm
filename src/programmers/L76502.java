d07abe8c2466d5c9929077b92b3333bf8a1f463d
JSoi <jsoi950715@gmail.com>
2024-09-06 11:24:01 +0900

    POG L76502 Solved

---
+++ src/programmers/L76502.java
@@ -0,0 +1,42 @@
+package programmers;
+
+import java.util.*;
+
+public class L76502 {
+    public static void main(String[] args) {
+        int answer = new L76502().solution("[(){{}}]");
+        System.out.println(answer);
+
+    }
+
+    public int solution(String s) {
+        int answer = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String target = s.substring(i) + s.substring(0, i);
+            answer += check(target) ? 1 : 0;
+        }
+        return answer;
+    }
+
+    boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            if ("{[(".contains(String.valueOf(c))) {
+                stack.push(c);
+            } else {
+                char opposite = switch (c) {
+                    case ')' -> '(';
+                    case ']' -> '[';
+                    case '}' -> '{';
+                    default -> throw new IllegalArgumentException();
+                };
+                if (stack.isEmpty() || stack.peek() != opposite) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
