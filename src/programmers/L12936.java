06aee288dd9192a26bcb932f50c0a9b6a35b59be
JSoi <jsoi950715@gmail.com>
2024-09-15 23:41:11 +0900

    POG L12936 TO Solved

--- src/programmers/L12936.java
+++ src/programmers/L12936.java
@@ -9,34 +9,33 @@ public class L12936 {
         System.out.println(Arrays.toString(new L12936().solution(3, 5)));
     }
 
-    static boolean[] visit;
-    static int[] temp;
-    static List<int[]> answerList;
-    static long targetSize;
-
     public static int[] solution(int n, long k) {
-        answerList = new ArrayList<>();
-        visit = new boolean[n];
-        temp = new int[n];
-        targetSize = k;
-        visit(0);
-        answerList.stream().map(Arrays::toString).forEach(System.out::println);
-        return answerList.get((int) k - 1);
+        List<Integer> candidates = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            candidates.add(i);
+        }
+        int[] answer = new int[n];
+        int index = 0;
+        long div = factorial(n);
+        while (!candidates.isEmpty()) {
+            div /= n--;
+            int pickIndex = (int) ((k - 1) / div);
+            int toUse = candidates.get(pickIndex);
+            candidates.remove(pickIndex);
+            k -= pickIndex * div;
+            answer[index++] = toUse;
+        }
+        return answer;
     }
 
-    private static void visit(int count) {
-//        System.out.println(count + " | "+ Arrays.toString(temp));
-        if (count >= temp.length) {
-            answerList.add(temp.clone());
-            return;
+    private static long factorial(int targetSize) {
+        if (targetSize <= 1) {
+            return 1;
         }
-        for (int i = 0; i < visit.length; i++) {
-            if (!visit[i] && answerList.size() < targetSize) {
-                visit[i] = true;
-                temp[count] = i + 1;
-                visit(count + 1);
-                visit[i] = false;
-            }
+        long answer = 1;
+        for (int i = 1; i <= targetSize; i++) {
+            answer *= i;
         }
+        return answer;
     }
 }
