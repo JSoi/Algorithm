cf6bda4b4ad284556c5fac155e49611c1df86eae
JSoi <jsoi950715@gmail.com>
2024-09-24 17:13:22 +0900

    PG L340211 Solved

--- src/programmers/L340211.java
+++ src/programmers/L340211.java
@@ -1,96 +1,78 @@
 package programmers;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class L340211 {
-    static final int[][] movement = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    static boolean[][] visit;
-    static int targetRow, targetCol, distance;
-    static List<Node> answerList;
-
     public static void main(String[] args) {
-        int solution = new L340211().solution(new int[][]{{3, 2}, {6, 4}, {4, 7}, {1, 4}}, new int[][]{{4, 2}, {1, 3}, {2, 4}});
+//        int solution = new L340211().solution(new int[][]{{3, 2}, {6, 4}, {4, 7}, {1, 4}}, new int[][]{{4, 2}, {1, 3}, {2, 4}});
+        int solution = new L340211().solution(new int[][]{{2, 2}, {2, 3}, {2, 7}, {6, 6}, {5, 2}}, new int[][]{{2, 3, 4, 5}, {1, 3, 4, 5}});
+        System.out.println("solution = " + solution);
     }
+
+    static HashMap<Point, Integer> pointOfTime;
+    static int targetRow, targetCol;
+    static int time;
 
     public static int solution(int[][] points, int[][] routes) {
-        int maxRow = Arrays.stream(points).mapToInt(p -> p[0]).max().orElse(0) + 1;
-        int maxCol = Arrays.stream(points).mapToInt(p -> p[1]).max().orElse(0) + 1;
-        visit = new boolean[maxRow][maxCol];
-        int routeIndex = 0;
-        HashMap<Integer, ArrayList<Node>> map = new HashMap<>();
+        pointOfTime = new HashMap<>();
         for (int[] route : routes) {
-            int fromPoint = route[0];
-            int toPoint = route[1];
-            Arrays.stream(visit).forEach(row -> Arrays.fill(row, false));
+            time = 1;
+            Point newPoint = new Point(points[route[0] - 1][0] - 1, points[route[0] - 1][1] - 1, time);
+            pointOfTime.put(newPoint, pointOfTime.getOrDefault(newPoint, 0) + 1);
+            for (int ri = 0; ri < route.length - 1; ri++) {
+                int fromPoint = route[ri];
+                int toPoint = route[ri + 1];
+                int fromRow = points[fromPoint - 1][0] - 1;
+                int fromCol = points[fromPoint - 1][1] - 1;
 
-            int fromCol = points[fromPoint - 1][1] - 1;
-            int fromRow = points[fromPoint - 1][0] - 1;
-
-            init(points, toPoint, fromCol, fromRow);
-            ArrayList<Node> routeList = new ArrayList<>();
-            routeList.add(new Node(fromRow, fromCol, 0));
-            explore(fromRow, fromCol, 0, routeList);
-            map.put(routeIndex++, new ArrayList<>(answerList));
+                targetRow = points[toPoint - 1][0] - 1;
+                targetCol = points[toPoint - 1][1] - 1;
+                addPoints(fromRow, fromCol);
+            }
         }
-        return findCollisionTime(map);
+        return (int) pointOfTime.entrySet().stream().filter(e -> e.getValue() > 1).count();
     }
 
-    private static void init(int[][] points, int toPoint, int fromCol, int fromRow) {
-        targetRow = points[toPoint - 1][0] - 1;
-        targetCol = points[toPoint - 1][1] - 1;
-        distance = Math.abs(targetCol - fromCol) + Math.abs(targetRow - fromRow) + 1;
-        visit[fromRow][fromCol] = true;
-    }
-
-    private static int findCollisionTime(HashMap<Integer, ArrayList<Node>> map) {
-        int size = map.values().stream().mapToInt(ArrayList::size).max().orElse(1);
-        int collision = 0;
-        for (int time = 0; time < size; time++) {
-            HashMap<Point, Integer> points = new HashMap<>();
-            for (ArrayList<Node> nodes : map.values()) {
-                if (nodes.size() <= time) {
-                    continue;
-                }
-
-                Point newPoint = new Point(nodes.get(time).row, nodes.get(time).col);
-                points.put(newPoint, points.getOrDefault(newPoint, 0) + 1);
+    private static void addPoints(int startR, int startC) {
+//        System.out.println(String.format("[%d %d] -> [%d %d]", startR, startC, targetRow, targetCol));
+        // 상 하 좌 우
+        Point newPoint;
+        while (startR != targetRow) {
+            if (startR < targetRow) { // down
+                newPoint = new Point(++startR, startC, ++time);
+            } else { // up
+                newPoint = new Point(--startR, startC, ++time);
             }
-            collision += (int) points.entrySet().stream().filter(e -> e.getValue() >= 2).count();
+            pointOfTime.put(newPoint, pointOfTime.getOrDefault(newPoint, 0) + 1);
         }
-        return collision;
-    }
-
-    private static void explore(int row, int col, int dist, List<Node> routeList) {
-        if (row == targetRow && col == targetCol) {
-            if (dist < distance) {
-                distance = dist;
-                answerList = routeList;
+        while (startC != targetCol) {
+            if (startC < targetCol) { // to right
+                newPoint = new Point(startR, ++startC, ++time);
+            } else { // to left
+                newPoint = new Point(startR, --startC, ++time);
             }
-            return;
-        }
-
-        for (int[] mv : movement) {
-            int nextRow = row + mv[0];
-            int nextCol = col + mv[1];
-            if (nextCol < 0 || nextCol >= visit[0].length || nextRow < 0 || nextRow >= visit.length
-                    || visit[nextRow][nextCol] || dist > distance) {
-                continue;
-            }
-            LinkedList<Node> subList = new LinkedList<>(routeList);
-            subList.add(new Node(nextRow, nextCol, dist + 1));
-            visit[nextRow][nextCol] = true;
-            explore(nextRow, nextCol, dist + 1, subList);
-            visit[nextRow][nextCol] = false;
+            pointOfTime.put(newPoint, pointOfTime.getOrDefault(newPoint, 0) + 1);
         }
     }
 
     private static class Point {
         int row;
         int col;
+        int time;
 
-        public Point(int row, int col) {
+        public Point(int row, int col, int time) {
             this.row = row;
             this.col = col;
+            this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + row +
+                    ", " + col +
+                    "] -> time=" + time +
+                    ']';
         }
 
         @Override
@@ -98,24 +80,12 @@ public class L340211 {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Point point = (Point) o;
-            return row == point.row && col == point.col;
+            return row == point.row && col == point.col && time == point.time;
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(row, col);
-        }
-    }
-
-    private static class Node {
-        int row;
-        int col;
-        int dist;
-
-        public Node(int row, int col, int dist) {
-            this.row = row;
-            this.col = col;
-            this.dist = dist;
+            return Objects.hash(row, col, time);
         }
     }
 }
