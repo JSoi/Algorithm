a4475a69dd700d0b80915ceb2012aabdea2cc932
JSoi <jsoi950715@gmail.com>
2024-09-30 15:13:17 +0900

    POG L12927 Solved

---
+++ src/programmers/L12927.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class L12927 {
+    // queue - 우선순위
+    public static void main(String[] args) {
+        long solution = new L12927().solution(1, new int[]{2, 1, 2});
+        System.out.println(solution);
+    }
+
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int work : works) {
+            queue.offer(work);
+        }
+        while (n > 0) {
+            int biggest = queue.poll();
+            if (biggest == 0) {
+                break;
+            }
+            queue.offer(biggest - 1);
+            n--;
+        }
+        while (!queue.isEmpty()) {
+            int value = queue.poll();
+            answer += (long) value * value;
+        }
+        return answer;
+    }
+
+}
