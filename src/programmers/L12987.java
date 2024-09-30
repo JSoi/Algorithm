bc7961fefc9290f2109da3e228bfd7bff1f48329
JSoi <jsoi950715@gmail.com>
2024-10-01 01:18:55 +0900

    POG L12987 Solved

---
+++ src/programmers/L12987.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class L12987 {
+
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int b : B) {
+            pq.offer(b);
+        }
+        for (int a : A) {
+            while (!pq.isEmpty() && pq.poll() > a) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
