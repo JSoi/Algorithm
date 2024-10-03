43948c821c6ee2db50120570578ef2006219dae0
JSoi <jsoi950715@gmail.com>
2024-10-03 11:12:33 +0900

    POG L67258 TO Solved

--- src/programmers/L67258.java
+++ src/programmers/L67258.java
@@ -1,8 +1,6 @@
 package programmers;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class L67258 {
     public static void main(String[] args) {
@@ -10,27 +8,31 @@ public class L67258 {
         System.out.println("solution = " + Arrays.toString(solution));
     }
 
-
     public int[] solution(String[] gems) {
+        Map<String, Integer> countMap = new HashMap<>();
+        int totalCount = (int) Arrays.stream(gems).distinct().count();
+        int start = 0;
+        int end = 0;
+        int minLen = Integer.MAX_VALUE;
         int[] answer = new int[2];
-        int minLength = Integer.MAX_VALUE;
-        Set<String> gemSet = new HashSet<>(Arrays.asList(gems));
-        for (int s = 0; s < gems.length; s++) {
-            HashSet<String> localGemSet = new HashSet<>();
-            for (int e = s; e < gems.length; e++) {
-                localGemSet.add(gems[e]);
-                if (localGemSet.size() == gemSet.size()) {
-                    if (minLength > e - s + 1) {
-                        minLength = e - s + 1;
-                        answer[0] = s + 1;
-                        answer[1] = e + 1;
-                    }
-                    break;
+        while (end < gems.length) {
+            countMap.put(gems[end], countMap.getOrDefault(gems[end], 0) + 1);
+            while (countMap.size() == totalCount) {
+                if (end - start + 1 < minLen) {
+                    minLen = end - start + 1;
+                    answer[0] = start + 1;
+                    answer[1] = end + 1;
                 }
+                int strValue = countMap.getOrDefault(gems[start], 0);
+                if (strValue > 1) {
+                    countMap.put(gems[start], strValue - 1);
+                } else {
+                    countMap.remove(gems[start]);
+                }
+                start++;
             }
+            end++;
         }
         return answer;
     }
-
-
 }
