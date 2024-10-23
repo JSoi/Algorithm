a96f431d706acb39c7701707ca8c2fc030c9b0dc
JSoi <jsoi950715@gmail.com>
2024-10-23 13:03:48 +0900

    POG L42897 TO

--- src/programmers/L42897.java
+++ src/programmers/L42897.java
@@ -4,50 +4,32 @@ import java.util.*;
 
 public class L42897 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Solution42897 sol = new Solution42897();
-		System.out.println(sol.solution(new int[] { 100, 1, 1, 100, 1 }));
-	}
+    public static void main(String[] args) {
+        int solution = new L42897().solution(new int[]{1, 2, 3, 1});
+//        int solution2 = new L42897().solution(new int[]{1, 2, 3});
+        System.out.println("solution = " + solution);
+    }
 
-}
-
-class Solution42897 {
-	int answer = 0;
-
-	public int solution(int[] money) { // 시간초과
-		steal(money, new boolean[money.length], 0, 0, 0);
-		return answer;
-	}
-
-	public void steal(int[] money, boolean[] sorn, int index, int stealCount, int stealMoney) {
-		if (!stealOK(sorn))
-			return;
-		if (stealCount >= (money.length / 2) + money.length % 2) {
-//			System.out.println(Arrays.toString(sorn));
-			answer = Math.max(answer, stealMoney);
-			return;
-		}
-		for (int i = index; i < money.length; i++) {
-			if (!sorn[i]) {
-				sorn[i] = true;
-				steal(money, sorn, index + 1, stealCount + 1, stealMoney + money[i]);
-				sorn[i] = false;
-				steal(money, sorn, index + 1, stealCount, stealMoney);
-			}
-		}
-	}
-
-	public boolean stealOK(boolean[] sorn) {
-		for (int i = 0; i < sorn.length; i++) {
-			int left = (i - 1 >= 0) ? (i - 1) : (sorn.length - 1);
-			int right = (i + 1) < sorn.length ? (i + 1) : 0;
-			if (sorn[i]) {
-				if (sorn[left] || sorn[right]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    public int solution(int[] money) {
+        int len = money.length;
+        int[][] dp = new int[len][2];
+        if (len <= 2) {
+            return Arrays.stream(money).max().orElse(0);
+        }
+        dp[0][1] = dp[1][0] = money[0];
+        for (int i = 2; i < dp.length - 1; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + money[i];
+        }
+        int withFirstHouse = Math.max(dp[len - 2][0], dp[len - 2][1]);
+        Arrays.fill(dp[0], 0);
+        Arrays.fill(dp[1], 0);
+        dp[1][1] = money[1];
+        for (int i = 2; i < dp.length - 1; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + money[i];
+        }
+        int withoutFirstHouse = Math.max(Math.max(dp[len - 2][0], dp[len - 2][1]), dp[len - 2][0] + +money[len - 1]);
+        return Math.max(withoutFirstHouse, withFirstHouse);
+    }
 }
