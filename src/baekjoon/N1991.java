b77eaa1619877cd73f9cc02f2076ab5fae7350c6
JSoi <jsoi950715@gmail.com>
2024-11-12 19:20:03 +0900

    BOJ N1302 Solved

---
+++ src/baekjoon/N1991.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+public class N1991 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Tree{
+        int current;
+        Tree left;
+        Tree right;
+
+
+    }
+}
