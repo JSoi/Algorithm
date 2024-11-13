292c72bf9eb83e70588f0f1dcd2d34d3960bfdfe
JSoi <jsoi950715@gmail.com>
2024-11-13 10:01:01 +0900

    BOJ N1120 Solved

---
+++ src/baekjoon/N1120.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N1120 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputStr = br.readLine().split(" ");
+        System.out.println(getMin(inputStr[0], inputStr[1]));
+    }
+
+    private static int getMin(String a, String b) {
+        int answer = Integer.MAX_VALUE;
+        for (int shift = 0; shift <= b.length() - a.length(); shift++) {
+            int tempAnswer = 0;
+            for (int aIndex = 0; aIndex < a.length(); aIndex++) {
+                if (a.charAt(aIndex) != b.charAt(aIndex+shift)) {
+                    tempAnswer++;
+                }
+            }
+            answer = Math.min(answer, tempAnswer);
+        }
+        return answer;
+    }
+}
