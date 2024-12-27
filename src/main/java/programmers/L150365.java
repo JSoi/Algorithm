ed9095eeb998ff89a4ec5c8ed1bc456d61188753
JSoi <jsoi950715@gmail.com>
2024-12-27 11:55:19 +0900

    POJ L150365 Solved

--- src/main/java/programmers/L150365.java
+++ src/main/java/programmers/L150365.java
@@ -1,19 +1,8 @@
 package programmers;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Optional;
-import java.util.Queue;
+import java.util.*;
 
 public class L150365 {
-    static final String IMP = "impossible";
-    static char[][] map;
-    static int moveCount;
-    static String answerStr;
-    static final int[][] move = new int[][]{{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
-    static final char[] moveChar = new char[]{'d', 'l', 'r', 'u'};
-
-    // d, l, r, u
     public static void main(String[] args) {
         String solution = new L150365().solution(3, 4, 2, 3, 3, 1, 5);
 //        String solution = new L150365().solution(2, 2, 1, 1, 2, 2, 2);
@@ -22,38 +11,59 @@ public class L150365 {
 
     }
 
+    static final String IMP = "impossible";
+    static char[][] map;
+    static int moveCount;
+    static String answerStr;
+    static final int[][] move = new int[][]{{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+    static boolean[][][] visit;
+    static final char[] moveChar = new char[]{'d', 'l', 'r', 'u'};
+
+    // d, l, r, u
+
+
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         moveCount = k;
         map = new char[n][m];
+        visit = new boolean[n][m][k + 1];
         for (char[] chars : map) {
             Arrays.fill(chars, '.');
         }
         map[x - 1][y - 1] = 'S';
         map[r - 1][c - 1] = 'E';
-        travel(x - 1, y - 1, k);
+        travel(x - 1, y - 1, r - 1, c - 1, k);
         return Optional.ofNullable(answerStr).orElse(IMP);
 
     }
 
-    static void travel(int startR, int startC, int travelCount) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(startR, startC, ""));
-        while (!queue.isEmpty()) {
-            Node latest = queue.poll();
-            if (latest.track.length() == travelCount && map[latest.r][latest.c] == 'E') {
-                answerStr = latest.track;
-                return;
-            }
-            for (int i = 0 ; i < move.length; i++) {
+    static void travel(int startR, int startC, int endR, int endC, int totalTravelCount) {
+        int currentR = startR;
+        int currentC = startC;
+        StringBuilder sb = new StringBuilder();
+        int travelCount = totalTravelCount;
+        while (travelCount > 0) {
+            for (int i = 0; i < move.length; i++) {
                 int[] m = move[i];
-                int nextR = latest.r + m[0];
-                int nextC = latest.c + m[1];
-                if (nextR < 0 || nextR >= map.length || nextC < 0 || nextC >= map[0].length || latest.track.length() >= travelCount) {
+                int nextR = currentR + m[0];
+                int nextC = currentC + m[1];
+                if (nextR < 0 || nextR >= map.length || nextC < 0 || nextC >= map[0].length ||
+                        !isPathLongEnough(nextR, nextC, endR, endC, travelCount)) {
                     continue;
                 }
-                queue.offer(new Node(nextR, nextC, latest.track + moveChar[i]));
+                currentR = nextR;
+                currentC = nextC;
+                sb.append(moveChar[i]);
+                break;
             }
+            travelCount--;
         }
+        answerStr = currentR == endR && currentC == endC ? sb.toString() : null;
+    }
+
+    private static boolean isPathLongEnough(int currentR, int currentC, int destR, int destC, int leftMovements) {
+        int btwR = Math.abs(currentR - destR);
+        int btwC = Math.abs(currentC - destC);
+        return leftMovements >= btwC + btwR;
     }
 
     static class Node {
