46e78010145407f0d31619288abea0357af191e7
JSoi <jsoi950715@gmail.com>
2025-01-02 12:14:21 +0900

    BOJ N15663 Solved V2

--- src/main/java/baekjoon/N15663.java
+++ src/main/java/baekjoon/N15663.java
@@ -4,20 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class N15663 {
-    static Set<String> answerSet;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(tok.nextToken());
         int R = Integer.parseInt(tok.nextToken());
         int[] arr = new int[N];
-        answerSet = new HashSet<>();
         tok = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(tok.nextToken());
@@ -29,15 +25,14 @@ public class N15663 {
     private static void permutation(int aIdx, boolean[] visit, int[] answer, int[] arr) {
         if (aIdx >= answer.length) {
             String answerStr = Arrays.stream(answer).mapToObj(String::valueOf).collect(Collectors.joining(" "));
-            if (!answerSet.contains(answerStr)) {
-                answerSet.add(answerStr);
-                System.out.println(answerStr);
-            }
+            System.out.println(answerStr);
             return;
         }
+        int latest = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (!visit[i]) {
+            if (!visit[i] && latest != arr[i]) {
                 visit[i] = true;
+                latest = arr[i];
                 answer[aIdx] = arr[i];
                 permutation(aIdx + 1, visit, answer, arr);
                 visit[i] = false;
