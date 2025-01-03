9de7bd39fee057fcef495de431ad5ec2e1bb7623
JSoi <jsoi950715@gmail.com>
2025-01-03 19:49:48 +0900

    BOJ N1092 Solved

--- src/main/java/baekjoon/N1092.java
+++ src/main/java/baekjoon/N1092.java
@@ -5,58 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class N1092 {
-    private static int[][] price;
-    private static int answer;
+    static int[][] dp;
+    static int[][] price;
+    static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int pplCount = Integer.parseInt(br.readLine());
-        price = new int[pplCount][pplCount];
-        for (int i = 0; i < pplCount; i++) {
+        N = Integer.parseInt(br.readLine());
+        price = new int[N][N];
+        for (int i = 0; i < N; i++) {
             char[] line = br.readLine().toCharArray();
-            for (int j = 0; j < pplCount; j++) {
+            for (int j = 0; j < N; j++) {
                 price[i][j] = line[j] - '0';
             }
         }
-        boolean[] possess = new boolean[pplCount];
-        possess[0] = true;
-        answer = 1;
-        sell(0, 0, possess);
-        System.out.println(answer);
+        dp = new int[1 << N][N + 1];
+        System.out.println(dfs(0, 0, 0) + 1);
     }
 
-    private static void sell(int owner, int currentPrice, boolean[] possess) {
-        // 판매 불가 시 업데이트
-        if (!ableToSell(currentPrice, price[owner], possess)) {
-            answer = Math.max(countCustomers(possess), answer);
-            return;
+    static int dfs(int owner, int currentPrice, int state) {
+        state |= 1 << owner;
+        if (dp[state][owner] > 0) {
+            return dp[state][owner];
         }
-        for (int i = 0; i < price.length; i++) {
-            if (possess[i] || price[owner][i] < currentPrice) {
-                continue;
-            }
-            possess[i] = true;
-            sell(i, price[owner][i], possess);
-            possess[i] = false;
-        }
-    }
-
-    private static int countCustomers(boolean[] possess) {
-        int count = 0;
-        for (boolean b : possess) {
-            if (b) {
-                count++;
+        for (int next = 0; next < N; next++) {
+            if ((state & (1 << next)) == 0 && price[owner][next] >= currentPrice) {
+                dp[state][owner] = Math.max(dp[state][owner], dfs(next, price[owner][next], state) + 1);
             }
         }
-        return count;
-    }
-
-    private static boolean ableToSell(int price, int[] customer, boolean[] possess) {
-        for (int i = 0; i < customer.length; i++) {
-            if (!possess[i] && customer[i] >= price) {
-                return true;
-            }
-        }
-        return false;
+        return dp[state][owner];
     }
 }
