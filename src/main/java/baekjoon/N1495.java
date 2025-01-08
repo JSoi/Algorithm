23ad15920ee954e0a92aaf3c76fb530fb7f9b3c3
JSoi <jsoi950715@gmail.com>
2025-01-08 15:03:10 +0900

    BOJ N1495 Solved

--- src/main/java/baekjoon/N1495.java
+++ src/main/java/baekjoon/N1495.java
@@ -13,19 +13,30 @@ public class N1495 {
         int startVolume = Integer.parseInt(tok.nextToken());
         int maxVolume = Integer.parseInt(tok.nextToken());
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        List<Integer>[] lists = new ArrayList[volumeCount + 1];
-        lists[0] = List.of(startVolume);
-        for (int i = 1; i < lists.length; i++) {
-            lists[i] = new ArrayList<>();
-            for (int v : lists[i - 1]) {
-                if (v + arr[i - 1] <= maxVolume) {
-                    lists[i].add(v + arr[i - 1]);
-                }
-                if (v - arr[i - 1] >= 0) {
-                    lists[i].add(v - arr[i - 1]);
+        boolean[] dp = new boolean[maxVolume + 1];
+        dp[startVolume] = true;
+        for (int i = 0; i < volumeCount; i++) {
+            int volumeChange = arr[i];
+            boolean[] nextDp = new boolean[maxVolume + 1];
+            for (int v = 0; v <= maxVolume; v++) {
+                if (dp[v]) {
+                    if (v + volumeChange <= maxVolume) {
+                        nextDp[v + volumeChange] = true;
+                    }
+                    if (v - volumeChange >= 0) {
+                        nextDp[v - volumeChange] = true;
+                    }
                 }
             }
+            dp = nextDp;
         }
-        System.out.println(lists[lists.length - 1].stream().mapToInt(Integer::valueOf).max().orElse(-1));
+
+        for (int v = maxVolume; v >= 0; v--) {
+            if (dp[v]) {
+                System.out.println(v);
+                return;
+            }
+        }
+        System.out.println(-1);
     }
 }
