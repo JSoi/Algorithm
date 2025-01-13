56fe10927de111a60f8b6bd898719cbe2f7b1ad3
JSoi <jsoi950715@gmail.com>
2025-01-13 11:42:38 +0900

    BOJ N1520 OOM

--- src/main/java/baekjoon/N1520.java
+++ src/main/java/baekjoon/N1520.java
@@ -5,52 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class N1520 {
-	static int count;
-	static int row, col;
-	static int map[][];
-	static int length;
-	static boolean[][] visit;
-	final static int[] dc = { 0, 0, -1, 1 };
-	final static int[] dr = { 1, -1, 0, 0 };
-	static BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+    static int row, col;
+    static int map[][];
+    static int answer;
+    static boolean[][] visit;
+    static final int[] dc = {0, 0, -1, 1};
+    static final int[] dr = {1, -1, 0, 0};
+    static BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
 
-	static void input() throws IOException {
-		String fLine = buf.readLine();
-		row = Integer.parseInt(fLine.split(" ")[0]);
-		col = Integer.parseInt(fLine.split(" ")[1]);
-		map = new int[row][col];
-		visit = new boolean[row][col];
-		for (int i = 0; i < row; i++) {
-			String thisLine = buf.readLine();
-			for (int j = 0; j < col; j++) {
-				map[i][j] = Integer.parseInt(thisLine.split(" ")[j]);
-			}
-		}
+    static void input() throws IOException {
+        String fLine = buf.readLine();
+        row = Integer.parseInt(fLine.split(" ")[0]);
+        col = Integer.parseInt(fLine.split(" ")[1]);
+        map = new int[row][col];
+        visit = new boolean[row][col];
+        for (int i = 0; i < row; i++) {
+            String thisLine = buf.readLine();
+            for (int j = 0; j < col; j++) {
+                map[i][j] = Integer.parseInt(thisLine.split(" ")[j]);
+            }
+        }
 
-	}
+    }
 
-	static void dfs(int r, int c, int value) {
-		System.out.println(r + "/" + c);
-		if ((r == row - 1) && (c == col - 1)) {
-			length++;
-		}
-		for (int k = 0; k < 4; k++) {
-			if (r + dr[k] < 0 || r + dr[k] >= row || c + dc[k] < 0 || c + dc[k] >= col || visit[r + dr[k]][c + dc[k]]
-					|| map[r + dr[k]][c + dc[k]] >= value) {
-				continue;
-			}
-			visit[r][c] = true;
-			dfs(r + dr[k], c + dc[k], map[r + dr[k]][c + dc[k]]);
-			visit[r][c] = false;
-		}
-	}
+    static void travel(int targetR, int targetC) {
+        if (targetR == map.length - 1 || targetC == map[0].length - 1) {
+            answer++;
+            return;
+        }
+        visit[targetR][targetC] = true;
+        for (int m = 0; m < 4; m++) {
+            int nextR = targetR + dr[m];
+            int nextC = targetC + dc[m];
+            if (nextC < 0 || nextC >= map[0].length || nextR < 0 || nextR >= map.length
+                    || map[nextR][nextC] >= map[targetR][targetC]) {
+                continue;
+            }
+            travel(nextR, nextC);
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		input();
-		length = 0;
-		dfs(0, 0, map[0][0]);
-		System.out.println(length);
-	}
-
+    public static void main(String[] args) throws IOException {
+        input();
+        travel(0, 0);
+        System.out.println(answer);
+    }
 }
