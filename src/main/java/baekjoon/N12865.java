ee4fdca2019845195d337143a91e7aa7ae7c1a2e
JSoi <jsoi950715@gmail.com>
2025-01-21 11:13:07 +0900

    BOJ N12865 solved

--- src/main/java/baekjoon/N12865.java
+++ src/main/java/baekjoon/N12865.java
@@ -4,30 +4,26 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class N12865 {
-	static int count;
-	static int weight;
-	static int[][] dp;
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		count = scan.nextInt();
-		weight = scan.nextInt();
-		dp = new int[count + 1][weight + 1];
-		for (int i = 1; i < count + 1; i++) {
-			int wVal = scan.nextInt(); // weight
-			int vVal = scan.nextInt(); // value
-
-			for (int j = 1; j < weight + 1; j++) {
-				if (j < wVal) {
-					dp[i][j] = dp[i-1][j];
-				}else {
-					dp[i][j] = Math.max(vVal + dp[i-1][j-wVal], dp[i-1][j]);
-				}
-			}
-		}
-		scan.close();
-		System.out.println(dp[count][weight]);
-	}
-
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
+        int weight = scan.nextInt();
+        int[] dp = new int[weight + 1];
+        for (int i = 0; i < count; i++) {
+            int wVal = scan.nextInt();
+            int vVal = scan.nextInt();
+            if (wVal > weight) {
+                continue;
+            }
+            for (int w = weight; w >= wVal; w--) {
+                dp[w] = Math.max(dp[w], dp[w - wVal] + vVal);
+            }
+        }
+        scan.close();
+        int answer = 0;
+        for (int w = 0; w <= weight; w++) {
+            answer = Math.max(answer, dp[w]);
+        }
+        System.out.println(answer);
+    }
 }
