5ae6a569aa46c264215ce50901ce5e598a8de1b5
JSoi <jsoi950715@gmail.com>
2025-01-21 11:36:02 +0900

    BOJ N1343 solved

--- src/main/java/baekjoon/N1343.java
+++ src/main/java/baekjoon/N1343.java
@@ -8,31 +8,27 @@ public class N1343 {
         String input = new Scanner(System.in).nextLine();
         ArrayList<String> strings = new ArrayList<>();
         StringBuilder temp = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
+        int i = 0;
+        while (i < input.length()) {
             char c = input.charAt(i);
-            if (temp.length() == 0) {
+            while (i < input.length() && input.charAt(i) == c) {
                 temp.append(c);
-                continue;
+                i++;
             }
-            if (temp.charAt(0) != c) {
-                strings.add(temp.toString());
-                temp = new StringBuilder(String.valueOf(c));
-            } else {
-                temp.append(c);
-                if (i == input.length() - 1) {
-                    strings.add(temp.toString());
-                }
+            String string = temp.toString();
+            if (string.startsWith("X") && string.length() % 2 == 1) {
+                System.out.println(-1);
+                return;
             }
+            strings.add(string);
+            temp.setLength(0);
         }
+//        System.out.println(strings);
         StringBuilder answer = new StringBuilder();
         for (String s : strings) {
             if (s.charAt(0) == '.') {
                 answer.append(s);
             } else {
-                if (s.length() % 2 == 1) {
-                    System.out.println(-1);
-                    return;
-                }
                 answer.append("AAAA".repeat(s.length() / 4))
                         .append("BB".repeat(s.length() % 4 > 0 ? 1 : 0));
             }
