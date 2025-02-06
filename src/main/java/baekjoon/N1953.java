70b3b1b745e55437681fde68f1963ce66d4b0c36
JSoi <jsoi950715@gmail.com>
2025-02-06 14:02:07 +0900

    BOJ N1953 solved

--- src/main/java/baekjoon/N1953.java
+++ src/main/java/baekjoon/N1953.java
@@ -3,52 +3,61 @@ package baekjoon;
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class N1953 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int ppl = Integer.parseInt(br.readLine());
-        Set<Integer>[] unlikeConn = new Set[ppl + 1];
-        for (int i = 0; i < unlikeConn.length; i++) {
-            unlikeConn[i] = new HashSet<>();
+        List<Integer>[] unlikeConn = new LinkedList[ppl + 1];
+        for (int i = 1; i < unlikeConn.length; i++) {
+            unlikeConn[i] = new LinkedList<>();
         }
         for (int l = 1; l <= ppl; l++) {
             StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
             int unlikeCount = Integer.parseInt(tok.nextToken());
-            if (unlikeCount == 0) {
-                continue;
+            while (unlikeCount-- > 0) {
+                int p = Integer.parseInt(tok.nextToken());
+                unlikeConn[l].add(p);
+                unlikeConn[p].add(l);
             }
-            int p = Integer.parseInt(tok.nextToken());
-            unlikeConn[l].add(p);
-            unlikeConn[p].add(l);
         }
         int[] visited = new int[ppl + 1];
+        List<Integer> whiteTeam = new ArrayList<>();
+        List<Integer> blackTeam = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
         for (int p = 1; p <= ppl; p++) {
             if (visited[p] != 0) {
                 continue;
             }
             visited[p] = 1;
+            whiteTeam.add(p);
             queue.offer(p);
             while (!queue.isEmpty()) {
                 int latest = queue.poll();
-                for (int i = 1; i <= ppl; i++) {
-                    if (visited[i] != 0) continue;
-                    if (unlikeConn[latest].contains(i)) {
-                        visited[i] = visited[latest] * -1;
+                for (int u : unlikeConn[latest]) {
+                    if (visited[u] != 0) continue;
+                    visited[u] = visited[latest] * -1;
+                    if (visited[u] == -1) {
+                        blackTeam.add(u);
+                    } else {
+                        whiteTeam.add(u);
                     }
+                    queue.offer(u);
                 }
             }
         }
 
-        int[] blackTeam = IntStream.rangeClosed(1, ppl).filter(p -> visited[p] == 1).toArray();
-        int[] whiteTeam = IntStream.rangeClosed(1, ppl).filter(p -> visited[p] == -1).toArray();
+        Collections.sort(whiteTeam);
+        Collections.sort(blackTeam);
+
+        if (blackTeam.isEmpty()) {
+            blackTeam.add(whiteTeam.remove(0));
+        }
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.append(String.valueOf(blackTeam.length)).append("\n");
-        bw.append(Arrays.stream(blackTeam).mapToObj(String::valueOf).collect(Collectors.joining(" "))).append("\n");
-        bw.append(String.valueOf(whiteTeam.length)).append("\n");
-        bw.append(Arrays.stream(whiteTeam).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
+        bw.append(String.valueOf(whiteTeam.size())).append("\n");
+        bw.append(whiteTeam.stream().map(String::valueOf).collect(Collectors.joining(" "))).append("\n");
+        bw.append(String.valueOf(blackTeam.size())).append("\n");
+        bw.append(blackTeam.stream().map(String::valueOf).collect(Collectors.joining(" ")));
         bw.flush();
     }
 }
