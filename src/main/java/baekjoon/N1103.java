3da6366d6f54c4b65869a6c63488bd86b74c6178
JSoi <jsoi950715@gmail.com>
2025-02-21 11:07:47 +0900

    BOJ N1103 solved

--- src/main/java/baekjoon/N1103.java
+++ src/main/java/baekjoon/N1103.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class N1103 {
-    private static final int MAX = Integer.MAX_VALUE;
     private static char[][] map;
-    private static int answer;
+    private static int[][] dp;
     private static final int[][] movement = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    private static boolean[][] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,38 +16,44 @@ public class N1103 {
         int row = Integer.parseInt(input[0]);
         int col = Integer.parseInt(input[1]);
         map = new char[row][col];
+        dp = new int[row][col];
         for (int r = 0; r < row; r++) {
             String line = br.readLine();
             for (int c = 0; c < col; c++) {
                 map[r][c] = line.charAt(c);
+                dp[r][c] = -1; // initialize
             }
         }
-        boolean[][] visit = new boolean[row][col];
-        visit[0][0] = true;
-        move(0, 0, 1, visit);
-        System.out.println(answer == MAX ? -1 : answer);
+        visit = new boolean[row][col];
+        System.out.println(findMaxMove(0, 0));
     }
 
-    private static void move(int r, int c, int moveCount, boolean[][] visited) {
-        // 방문한 곳을 또 방문할 시 무한으로 반복 가능
-        if (answer == MAX) {
-            return;
+    private static int findMaxMove(int r, int c) {
+        if (r < 0 || r >= map.length || c < 0 || c >= map[0].length || map[r][c] == 'H') {
+            // 방문할 수 없는 경우
+            return 0;
         }
-        answer = Math.max(answer, moveCount);
+        if (visit[r][c]) { // 방문한 곳을 또 방문한 경우
+            return -1;
+        }
+        if (dp[r][c] != -1) { // 이전에 계산한 위치
+            return dp[r][c];
+        }
+        int offset = map[r][c] - '0';
+        int maxMoves = 0;
+        visit[r][c] = true;
         for (int[] m : movement) {
-            int offset = (map[r][c] - '0');
             int nR = r + m[0] * offset;
             int nC = c + m[1] * offset;
-            if (answer == -1 || nR < 0 || nR >= map.length || nC < 0 || nC >= map[0].length || map[nR][nC] == 'H') {
-                continue;
+            int move = findMaxMove(nR, nC);
+            if (move == -1) { // 무한
+                return -1;
             }
-            if (visited[nR][nC]) {
-                answer = MAX;
-            } else {
-                visited[nR][nC] = true;
-                move(nR, nC, moveCount + 1, visited);
-                visited[nR][nC] = false;
-            }
+            maxMoves = Math.max(maxMoves, move);
         }
+        visit[r][c] = false;
+        // 모든 계산이 끝난 후 DP 값 할당
+        dp[r][c] = maxMoves + 1;
+        return dp[r][c];
     }
 }
