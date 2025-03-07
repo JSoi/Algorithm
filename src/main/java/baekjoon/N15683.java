8cde3ed9e2d14311a8036bd3ad4cf1683aaac6bd
JSoi <jsoi950715@gmail.com>
2025-03-07 13:13:16 +0900

    BOJ N15683 solved

--- src/main/java/baekjoon/N15683.java
+++ src/main/java/baekjoon/N15683.java
@@ -13,15 +13,17 @@ public class N15683 {
     private static List<int[]> cameraList;
     private static int row;
     private static int col;
-    private static int answer = Integer.MAX_VALUE;
-    private static final List<List<int[][]>> movements =
-            List.of(
-                    List.of(new int[][]{{0, 1}}, new int[][]{{0, -1}}, new int[][]{{-1, 0}}, new int[][]{{1, 0}}),
-                    List.of(new int[][]{{0, 1}, {0, -1}}, new int[][]{{1, 0}, {-1, 0}}),
-                    List.of(new int[][]{{0, 1}, {-1, 0}}, new int[][]{{0, -1}, {1, 0}}, new int[][]{{1, 0}, {0, -1}}, new int[][]{{-1, 0}, {0, 1}}),
-                    List.of(new int[][]{{0, 1}, {-1, 0}, {1, 0}}, new int[][]{{0, -1}, {1, 0}, {0, 1}}, new int[][]{{1, 0}, {0, -1}, {-1, 0}}, new int[][]{{-1, 0}, {0, 1}, {0, -1}}),
-                    List.of(new int[][][]{new int[][]{{0, 1}, {-1, 0}, {1, 0}, {0, -1}}})
-            );
+    private static int answer;
+
+    public static int[] dr = {-1, 1, 0, 0};
+    public static int[] dc = {0, 0, -1, 1};
+
+    public static int[][][] cctv = {{{0}},
+            {{0}, {1}, {2}, {3}},
+            {{2, 3}, {0, 1}},
+            {{0, 3}, {1, 3}, {1, 2}, {0, 2}},
+            {{0, 2, 3}, {0, 1, 3}, {1, 2, 3}, {0, 1, 2}},
+            {{0, 1, 2, 3}}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,22 +35,23 @@ public class N15683 {
         for (int r = 0; r < row; r++) {
             StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
             for (int c = 0; c < col; c++) {
-                int value = Integer.parseInt(tok.nextToken());
-                map[(r * col) + c] = value;
-                if (value < 6 && value > 0) {
-                    cameraList.add(new int[]{r, c, value});
+                int type = Integer.parseInt(tok.nextToken());
+                map[(r * col) + c] = type;
+                if (type < 6 && type > 0) {
+                    cameraList.add(new int[]{r, c, type});
                 }
             }
         }
+        answer = (int) Arrays.stream(map).filter(m -> m == 0).count();
         go(0);
         System.out.println(answer);
     }
 
     private static void travel(int curR, int curC, int rDir, int cDir) {
+        curR += rDir;
+        curC += cDir;
         while (curR >= 0 && curR < row && curC >= 0 && curC < col && map[curR * col + curC] != 6) {
-            if (map[curR * col + curC] == 0) {
-                map[curR * col + curC] = -1;
-            }
+            map[curR * col + curC] = -1;
             curR += rDir;
             curC += cDir;
         }
@@ -56,8 +59,8 @@ public class N15683 {
 
     private static void go(int depth) {
         if (depth == cameraList.size()) {
-            answer = Math.min((int) Arrays.stream(map).filter(m -> m == 0).count(), answer);
-//            System.out.println(print());
+            int count = (int) Arrays.stream(map).filter(m -> m == 0).count();
+            answer = Math.min(count, answer);
             return;
         }
         int[] cameraInfo = cameraList.get(depth);
@@ -65,11 +68,12 @@ public class N15683 {
         int c = cameraInfo[1];
         int cameraType = cameraInfo[2];
         int[] original = Arrays.copyOf(map, map.length);
-        for (int[][] m : movements.get(cameraType - 1)) {
-            for (int[] innerM : m) {
-                travel(r, c, innerM[0], innerM[1]);
-                go(depth + 1);
+        for (int i = 0; i < cctv[cameraType].length; i++) {
+            for (int j = 0; j < cctv[cameraType][i].length; j++) {
+                int dir = cctv[cameraType][i][j];
+                travel(r, c, dr[dir], dc[dir]);
             }
+            go(depth + 1);
             System.arraycopy(original, 0, map, 0, map.length);
         }
     }
