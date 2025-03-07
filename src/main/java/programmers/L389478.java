cbf75a0f060888f66cf0dd3d229ae95c93b59f23
JSoi <jsoi950715@gmail.com>
2025-03-07 14:14:33 +0900

    POG L389478 solved

---
+++ src/main/java/programmers/L389478.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class L389478 {
+    public static void main(String[] args) {
+        new L389478().solution(22, 6, 8);
+    }
+
+    public int solution(int n, int w, int num) {
+        int[] count = new int[w];
+        for (int i = 1; i <= n; i++) {
+            int col = (i - 1) % w;
+            int row = (i - 1) / w;
+            count[row % 2 == 0 ? col : w - col - 1]++;
+        }
+        int col = (num - 1) % w;
+        int row = (num - 1) / w;
+        return  count[row % 2 == 0 ? col : w - col - 1] - row;
+    }
+}
