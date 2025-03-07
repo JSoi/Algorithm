3b53a199d9bf427ce4db80ff70d6c779e015b0fc
JSoi <jsoi950715@gmail.com>
2025-03-07 13:59:30 +0900

    POG L388351 solved

---
+++ src/main/java/programmers/L388351.java
@@ -0,0 +1,37 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class L388351 {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int n = schedules.length;
+        boolean[] treasured = new boolean[n];
+        Arrays.fill(treasured, true);
+        for (int t = 0; t < 7; t++) {
+            int day = (startday + t - 1) % 7 + 1;
+            if (day == 6 || day == 7) {
+                continue;
+            }
+            for (int c = 0; c < n; c++) {
+                if (!treasured[c]) {
+                    continue;
+                }
+                if (toMinute(timelogs[c][t]) > toMinute(schedules[c]) + 10) {
+                    treasured[c] = false;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (boolean b : treasured) {
+            if (b) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    private int toMinute(int input) {
+        return 60 * (input / 100) + input % 100;
+    }
+}
