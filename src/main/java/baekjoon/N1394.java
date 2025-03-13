78f694c4a049c006ead2123e2ea7456a065a3336
JSoi <jsoi950715@gmail.com>
2025-03-13 09:30:39 +0900

    BOJ N1394 solved

--- src/main/java/baekjoon/N1394.java
+++ src/main/java/baekjoon/N1394.java
@@ -3,31 +3,42 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class N1394 {
+    public static final int DIV = 900528;
+    public static int cLen;
+    public static Map<Integer, Long> powMap;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        powMap = new HashMap<>();
         String givenPasswordChar = br.readLine();
         String password = br.readLine();
-        if (password.length() == 1) {
-            System.out.println(givenPasswordChar.indexOf(password.charAt(0)) + 1);
-            return;
-        }
+        int n = password.length();
+        cLen = givenPasswordChar.length();
+        powMap.put(0, 1L);
+        powMap.put(1, (long) cLen);
         long answer = 0;
-        for (int i = 0; i < password.length() - 1; i++) {
-            long val = 1;
-            for (int j = 0; j <= i; j++) {
-                val = (val * givenPasswordChar.length()) % 900528;
-            }
-            answer += val;
-            answer %= 900528;
+        for (int i = 0; i < n - 1; i++) {
+            answer = (answer + customPow(i + 1)) % DIV;
         }
-        long attempt = 1;
-        for (int i = 0; i < password.length(); i++) {
-            int k = givenPasswordChar.indexOf(password.charAt(i)) + 1;
-            attempt = (attempt * k) % 900528;
+        long attempt = 0;
+        for (int i = 0; i < n; i++) {
+            int k = givenPasswordChar.indexOf(password.charAt(i));
+            attempt += (customPow(n - i - 1) * k) % DIV;
         }
-        answer += attempt;
+        answer = (answer + attempt + 1) % DIV;
         System.out.println(answer);
+    }
+
+    private static long customPow(int pow) {
+        if (powMap.get(pow) != null) {
+            return powMap.get(pow);
+        }
+        long value = cLen * customPow(pow - 1) % DIV;
+        powMap.put(pow, value);
+        return value;
     }
 }
