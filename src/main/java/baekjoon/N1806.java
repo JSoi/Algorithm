89885f1bb685793f07243afd2f84ab1ed1e69c4c
JSoi <jsoi950715@gmail.com>
2025-03-18 08:48:45 +0900

    BOJ N1806 solved

--- src/main/java/baekjoon/N1806.java
+++ src/main/java/baekjoon/N1806.java
@@ -5,35 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * <a href ="https://www.acmicpc.net/problem/1806">부분합</a>
+ */
 public class N1806 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(tok.nextToken());
         int S = Integer.parseInt(tok.nextToken());
-        int answer = N + 1;
         tok = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[N];
-        long[] sum = new long[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(tok.nextToken());
         }
-        sum[0] = arr[0];
-        for (int i = 1; i < N; i++) {
-            sum[i] += sum[i - 1] + arr[i];
-            if (sum[i] == S) {
-                answer = Math.min(answer, i + 1);
-                continue;
-            }
-            if (sum[i] > S) {
-                for (int start = i - 1; start >= Math.max(0, i - answer); start--) {
-                    if (sum[i] - sum[start] >= S) {
-                        answer = Math.min(answer, i - start);
-                        break;
-                    }
-                }
+        int left = 0, sum = 0, minLength = Integer.MAX_VALUE;
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+            while (sum >= S) {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= arr[left++];
             }
         }
-        System.out.println(answer == N+1? 0 : answer);
+        System.out.println(minLength == Integer.MAX_VALUE ? 0 : minLength);
     }
 }
