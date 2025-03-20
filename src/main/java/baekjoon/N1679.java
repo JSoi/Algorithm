d79d2fe9364bf7c083032abb5e13e91a3d50f047
JSoi <jsoi950715@gmail.com>
2025-03-21 07:48:06 +0900

    BOJ N1679 refactor

--- src/main/java/baekjoon/N1679.java
+++ src/main/java/baekjoon/N1679.java
@@ -10,23 +10,23 @@ public class N1679 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int givenGameNum = Integer.parseInt(br.readLine());
-        int MAX = 100001;
-        int[] dp = new int[MAX];
-        Arrays.fill(dp, MAX);
-        dp[0] = 0;
         StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
         int k = Integer.parseInt(br.readLine());
         int[] nums = new int[givenGameNum];
         for (int i = 0; i < givenGameNum; i++) {
             nums[i] = Integer.parseInt(tok.nextToken());
         }
+        final int MAX = nums[nums.length - 1] * k + 1;
+        int[] dp = new int[MAX + 1];
+        Arrays.fill(dp, MAX);
+        dp[0] = 0;
         for (int num : nums) {
             dp[num] = 1;
-            for (int j = num + 1; j < MAX; j++) {
+            for (int j = num + 1; j <= MAX; j++) {
                 dp[j] = Math.min(dp[j], dp[j - num] + dp[num]);
             }
         }
-        for (int i = 1; i < MAX; i++) {
+        for (int i = 1; i <= MAX; i++) {
             if (dp[i] > k) {
                 System.out.println((i % 2 == 0 ? "holsoon" : "jjaksoon") + " win at " + i);
                 return;
