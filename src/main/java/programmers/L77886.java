f5ccf40fee2baa3da5884770f46a2da139450de5
JSoi <jsoi950715@gmail.com>
2025-03-27 11:25:43 +0900

    POG L77886 refactored

--- src/main/java/programmers/L77886.java
+++ src/main/java/programmers/L77886.java
@@ -15,46 +15,28 @@ public class L77886 {
     }
 
     private String replacedResult(String s) {
-        StringBuffer com = new StringBuffer();
         String bf = s;
         s = remove110(s);
-        com.append("110".repeat((bf.length() - s.length()) / 3));
+        String com = "110".repeat((bf.length() - s.length()) / 3);
         if (s.length() == 0) {
-            return com.toString();
+            return com;
         }
-        if (s.substring(0, Math.min(3, s.length())).equals("1")) {
-            return com.append("1").toString();
-        }
-        int pos = s.indexOf("11");
         int fZero = s.lastIndexOf("0");
-        int insertPos;
-        if (pos == -1) { // 10, 01, 0, 101, 00,
-            insertPos = fZero + 1;
-        } else {
-            insertPos = pos;
-        }
+        int insertPos = fZero == -1 ? 0 : fZero + 1;
         return s.substring(0, insertPos) + com + s.substring(insertPos);
     }
 
     public static String remove110(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            stack.push(c);
-            if (stack.size() >= 3) {
-                char third = stack.pop();
-                char second = stack.pop();
-                char first = stack.pop();
-                if (!(first == '1' && second == '1' && third == '0')) {
-                    stack.push(first);
-                    stack.push(second);
-                    stack.push(third);
-                }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            if (sb.length() >= 3 &&
+                    sb.charAt(sb.length() - 3) == '1' &&
+                    sb.charAt(sb.length() - 2) == '1' &&
+                    sb.charAt(sb.length() - 1) == '0') {
+                sb.delete(sb.length() - 3, sb.length());
             }
         }
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
-        return result.toString();
+        return sb.toString();
     }
 }
