65f65d1e541dc603c8adb0706a0af022f68ab0a2
JSoi <jsoi950715@gmail.com>
2025-04-04 15:11:35 +0900

    BOJ N1034 solved

--- src/main/java/baekjoon/N1034.java
+++ src/main/java/baekjoon/N1034.java
@@ -3,78 +3,40 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class N1034 {
-    private static int row;
-    private static int col;
-    private static boolean[] lightStatus;
-    private static boolean[] onRow;
-    private static int answer;
-    private static int k;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        row = Integer.parseInt(line[0]);
-        col = Integer.parseInt(line[1]);
-        lightStatus = new boolean[row * col];
-        onRow = new boolean[row];
-        Arrays.fill(onRow, true);
-        for (int r = 0; r < row; r++) {
-            String l = br.readLine();
-            for (int c = 0; c < col; c++) {
-                lightStatus[r * col + c] = l.charAt(c) == '1';
+        String[] split = br.readLine().split(" ");
+        int row = Integer.parseInt(split[0]);
+        int col = Integer.parseInt(split[1]);
+
+        List<String> lamps = new ArrayList<>();
+        Map<String, Integer> patternCount = new HashMap<>();
+
+        for (int i = 0; i < row; i++) {
+            String line = br.readLine();
+            lamps.add(line);
+            patternCount.put(line, patternCount.getOrDefault(line, 0) + 1);
+        }
+        int k = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (String pattern : patternCount.keySet()) {
+            int offCount = countZeros(pattern);
+            // 꺼진 개수가 K 이하이고, (K - 꺼진 수)가 짝수
+            if (offCount <= k && (k - offCount) % 2 == 0) {
+                answer = Math.max(answer, patternCount.get(pattern));
             }
         }
-
-        for (int c = 0; c < col; c++) {
-            for (int r = 0; r < row; r++) {
-                if (!lightStatus[r * col + c]) {
-                    onRow[r] = false;
-                    break;
-                }
-            }
-        }
-
-        k = Integer.parseInt(br.readLine()) % ((int) Math.pow(2, col));
-        findSuitableCol(0);
         System.out.println(answer);
     }
 
-    private static void findSuitableCol(int count) {
-        if (count == k) {
-            int onRowCount = findOnRowCount();
-            answer = Math.max(onRowCount, answer);
-            return;
-        }
-        for (int i = 0; i < col; i++) {
-            boolean[] temp = Arrays.copyOf(lightStatus, lightStatus.length);
-            changeColumnStatus(i);
-            findSuitableCol(count + 1);
-            lightStatus = temp;
-        }
-    }
-
-    private static int findOnRowCount() {
+    private static int countZeros(String s) {
         int count = 0;
-        for (int r = 0; r < row; r++) {
-            int c;
-            for (c = 0; c < col; c++) {
-                if (!lightStatus[r * col + c]) {
-                    break;
-                }
-            }
-            if (c == col) {
-                count++;
-            }
+        for (char ch : s.toCharArray()) {
+            if (ch == '0') count++;
         }
         return count;
-    }
-
-    private static void changeColumnStatus(int c) {
-        for (int r = 0; r < row; r++) {
-            lightStatus[r * col + c] = !lightStatus[r * col + c];
-        }
     }
 }
