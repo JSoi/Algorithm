45b6cd9cacf9d4e649e2c9884b97492d6c9c4c30
JSoi <jsoi950715@gmail.com>
2025-04-12 22:11:01 +0900

    BOJ N1301 OOM

--- src/main/java/baekjoon/N1301.java
+++ src/main/java/baekjoon/N1301.java
@@ -3,6 +3,8 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <a href = "https://www.acmicpc.net/problem/1301">비즈 공예</a>
@@ -10,9 +12,8 @@ import java.io.InputStreamReader;
 public class N1301 {
     static int totalBeadCount;
     static int[] beadArr;
-    static int answer = 0;
     static int[] used;
-
+    static Map<String, Integer> beadMap = new HashMap<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int beadColorCount = Integer.parseInt(br.readLine()); // [3,5]
@@ -24,25 +25,27 @@ public class N1301 {
             beadArr[b] = count;
             totalBeadCount += count;
         }
-        answer = 0;
+        int answer = 0;
         for (int i = 0; i < beadColorCount; i++) {
             used[i]++;
-            fillBeads(1, -1, i);
+            answer += fillBeads(1, "", -1, i);
             used[i]--;
         }
         System.out.println(answer);
     }
 
-    static void fillBeads(int usedBeads, int first, int second) {
-        if (usedBeads == totalBeadCount) {
-            answer++;
-            return;
-        }
+    static int fillBeads(int usedBeads, String status, int last1, int last2) {
+        if (usedBeads == totalBeadCount) return 1;
+        String key = status + (last1 == -1 ? "" : String.valueOf(last1)) + last2;
+        if (beadMap.containsKey(key)) return beadMap.get(key);
+        int total = 0;
         for (int i = 0; i < beadArr.length; i++) {
-            if (used[i] >= beadArr[i] || i == first || i == second) continue; // 중복된 색상
+            if (used[i] >= beadArr[i] || i == last1 || i == last2) continue;
             used[i]++;
-            fillBeads(usedBeads + 1, second, i);
+            total += fillBeads(usedBeads + 1, status + "" + i, last2, i);
             used[i]--;
         }
+        beadMap.put(key, total);
+        return total;
     }
 }
