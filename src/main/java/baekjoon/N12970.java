184432ed1abb3ce1ff6d52177c664fa252b7bdd7
JSoi <jsoi950715@gmail.com>
2025-04-28 20:36:08 +0900

    BOJ N12970 solving

--- src/main/java/baekjoon/N12970.java
+++ src/main/java/baekjoon/N12970.java
@@ -1,29 +1,38 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class N12970 {
-    private static int k;
-    private static int n;
-    private static String answer = "";
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-        k = scan.nextInt();
-        scan.close();
-        count("", 0, 0);
-        System.out.println(answer.isEmpty() ? -1 : answer);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        char[] s = new char[n];
+        Arrays.fill(s, 'B');
+
+        int aCount = 0;
+        for (int i = 0; i < n; i++) {
+            int cCount = n - i - 1;
+            if (k >= cCount) {
+                s[i] = 'A';
+                k -= (cCount - aCount);
+                aCount++;
+            } else {
+                int targetIdx = n - k + aCount - 1;
+                if (targetIdx >= i && targetIdx < n) {
+                    s[targetIdx] = 'A';
+                    k = 0;
+                }
+                break;
+            }
+        }
+        if (k != 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(new String(s));
+        }
     }
 
-    static void count(String curr, int bCount, int conditionCount) {
-        if (curr.length() == n) {
-            if (conditionCount == k) {
-                answer = curr;
-            }
-            return;
-        }
-        count("A" + curr, bCount, conditionCount + bCount);
-        count("B" + curr, bCount + 1, conditionCount);
-    }
 }
