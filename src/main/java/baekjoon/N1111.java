7af15adece309ecb9e55471f364aa2bbe37d69af
JSoi <jsoi950715@gmail.com>
2025-05-01 09:53:13 +0900

    BOJ N1111 solved

--- src/main/java/baekjoon/N1111.java
+++ src/main/java/baekjoon/N1111.java
@@ -1,6 +1,5 @@
 package baekjoon;
 
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class N1111 {
@@ -12,58 +11,27 @@ public class N1111 {
             arr[i] = scan.nextInt();
         }
         scan.close();
-        HashSet<Pair> pairs = new HashSet<>();
-        if (arr.length < 2) {
-            System.out.println("A");
+        if (arr.length <= 2) {
+            if (arr.length == 1) {
+                System.out.println("A");
+            } else if (arr[0] == arr[1]) {
+                System.out.println(arr[0]);
+            } else {
+                System.out.println("A");
+            }
             return;
         }
-        // init
-        int a = arr[0];
-        int b = arr[1];
-        for (int i = -100; i <= 100; i++) {
-            for (int j = -100; j <= 100; j++) {
-                if (i * a + j == b) {
-                    pairs.add(new Pair(i, j));
+        for (int i = -200; i <= 200; i++) {
+            int num = arr[1] - arr[0] * i;
+            int cnt = 2;
+            while (arr[cnt] - arr[cnt - 1] * i == num) {
+                if (cnt == n - 1) {
+                    System.out.println(arr[n - 1] * i + num);
+                    return;
                 }
+                cnt++;
             }
         }
-        for (int k = 2; k < n; k++) {
-            int aa = arr[k - 1];
-            int bb = arr[k];
-            pairs.removeIf(pair -> pair.a * aa + pair.b != bb);
-            if (pairs.isEmpty()) {
-                System.out.println("B");
-                return;
-            }
-        }
-        HashSet<Integer> answerSet = new HashSet<>();
-
-            int lastE = arr[arr.length - 1];
-        for (Pair pair : pairs) {
-            answerSet.add(pair.a * lastE + pair.b);
-        }
-        if (answerSet.size() > 1) {
-            System.out.println("A");
-        } else {
-            System.out.println(answerSet.stream().findFirst().get());
-        }
-
-    }
-
-    private static class Pair {
-        int a;
-        int b;
-
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass()) return false;
-            Pair pair = (Pair) o;
-            return a == pair.a && b == pair.b;
-        }
-
-        public Pair(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
+        System.out.println("B");
     }
 }
