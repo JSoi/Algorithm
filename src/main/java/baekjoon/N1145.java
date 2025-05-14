c95347f550ab5c319495eaee0e952f92ef9955a8
JSoi <jsoi950715@gmail.com>
2025-05-14 12:26:02 +0900

    BOJ N1145 solved

---
+++ src/main/java/baekjoon/N1145.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N1145 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int answer = 1;
+        while (true) {
+            int count = 0;
+            for (int j : arr) {
+                if (answer % j == 0) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                break;
+            }
+            answer++;
+        }
+        System.out.println(answer);
+    }
+}
