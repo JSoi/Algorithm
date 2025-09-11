b6f31d34ef1503c0167774f7f7994181e4d38f76
JSoi <jsoi950715@gmail.com>
2025-09-11 19:23:44 +0900

    BOJ N1455 solved

--- java/src/main/java/com/soi/baekjoon/N1455.java
+++ java/src/main/java/com/soi/baekjoon/N1455.java
@@ -1,31 +1,32 @@
 package com.soi.baekjoon;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class N1455 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] line = br.readLine().split(" ");
-        int row = Integer.parseInt(line[1]);
-        int col = Integer.parseInt(line[0]);
-        int[][] map = new int[row][col];
-        int[][] flipMap = new int[row][col];
+        int row = Integer.parseInt(line[0]);
+        int col = Integer.parseInt(line[1]);
+        boolean[][] map = new boolean[row][col];
+        boolean[][] flipMap = new boolean[row][col];
         for (int i = 0; i < row; i++) {
             String mapLine = br.readLine();
             for (int j = 0; j < col; j++) {
-                map[i][j] = mapLine.charAt(j) - '0';
+                map[i][j] = mapLine.charAt(j) - '0' == 1;
             }
         }
         br.close();
         int flipCount = 0;
         for (int r = row - 1; r >= 0; r--) {
             for (int c = col - 1; c >= 0; c--) {
-                if (map[r][c] != (flipMap[r][c] % 2 == 0 ? 0 : 1)) {
+                if (flipMap[r][c] != map[r][c]) {
                     flipCount++;
                     for (int rr = 0; rr <= r; rr++) {
                         for (int cc = 0; cc <= c; cc++) {
-                            flipMap[rr][cc]++;
+                            flipMap[rr][cc] = !flipMap[rr][cc];
                         }
                     }
                 }
