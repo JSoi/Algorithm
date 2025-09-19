fb0c054bb5e39fdaf7f3df8df1c1076e1bdcd4e6
JSoi <jsoi950715@gmail.com>
2025-09-20 02:14:21 +0900

    BOJ N11726 solved

--- java/src/main/java/com/soi/baekjoon/N11726.java
+++ java/src/main/java/com/soi/baekjoon/N11726.java
@@ -3,21 +3,16 @@ package com.soi.baekjoon;
 import java.util.Scanner;
 
 public class N11726 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		scan.close();
-		int[] dp = new int[n];
-		dp[0] = 1;
-		if (n != 1) {
-			dp[1] = 2;
-		}
-		for (int i = 2; i <= n - 1; i++) {
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-		}
-		System.out.println(dp[n - 1]);
-	}
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        final int div = 10007;
+        int n = scan.nextInt();
+        scan.close();
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % div;
+        }
+        System.out.println(dp[n]);
+    }
 }
