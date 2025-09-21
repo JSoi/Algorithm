fe5e7f7686b8a5017bbba9c4928547dd2403bc61
JSoi <jsoi950715@gmail.com>
2025-09-21 21:28:14 +0900

    BOJ N1149 solved

---
+++ java/src/main/java/com/soi/baekjoon/N1149_2.java
@@ -0,0 +1,23 @@
+package com.soi.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class N1149_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] cost = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            cost[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        for (int i = 1; i < n; i++) {
+            cost[i][0] += Math.min(cost[i-1][1], cost[i-1][2]);
+            cost[i][1] += Math.min(cost[i-1][0], cost[i-1][2]);
+            cost[i][2] += Math.min(cost[i-1][0], cost[i-1][1]);
+        }
+        System.out.println(Math.min(Math.min(cost[n - 1][0], cost[n - 1][1]), cost[n - 1][2]));
+    }
+}
