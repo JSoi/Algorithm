9bccc7231c2f011f2d294ec946f95b443db58768
JSoi <jsoi950715@gmail.com>
2025-09-25 15:06:52 +0900

    BOJ N1932 solved

---
+++ java/src/main/java/com/soi/baekjoon/N1932_2.java
@@ -0,0 +1,33 @@
+package com.soi.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N1932_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer tok = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = Integer.parseInt(tok.nextToken());
+            }
+        }
+        dp[0][0] = arr[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = arr[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = arr[i][j] + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
+            }
+        }
+        System.out.println(Arrays.stream(dp[n - 1]).max().getAsInt());
+    }
+}
