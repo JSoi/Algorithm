d116fba32c4be9a8f9eba8b331ade1f7969484a2
JSoi <jsoi950715@gmail.com>
2025-10-02 10:57:39 +0900

    BOJ N18352 solved

--- java/src/main/java/com/soi/baekjoon/N18352.java
+++ java/src/main/java/com/soi/baekjoon/N18352.java
@@ -3,8 +3,7 @@ package com.soi.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class N18352 {
     public static final int MAX = Integer.MAX_VALUE;
@@ -15,36 +14,45 @@ public class N18352 {
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken()) - 1;
-        int[][] conn = new int[N][N];
-        for (int[] c : conn) {
-            Arrays.fill(c, MAX);
-        }
+        List<Integer>[] conn = new List[N];
         for (int i = 0; i < N; i++) {
-            conn[i][i] = 0;
+            conn[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken()) - 1;
             int b = Integer.parseInt(st.nextToken()) - 1;
-            conn[a][b] = 1;
+            conn[a].add(b);
         }
-        for (int k = 0; k < K; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if ( conn[i][k] == MAX || conn[k][j] == MAX) {
-                        continue;
-                    }
-                    conn[i][j] = Math.min(conn[i][j], conn[i][k] + conn[k][j]);
+        int[] dist = new int[N]; // x 로부터의 거리
+        boolean[] visit = new boolean[N]; // 방문 여부
+        Arrays.fill(dist, MAX);
+//        for (List<Integer> c : conn) {
+//            System.out.println(c);
+//        }
+        dist[X] = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{X, 0});
+        visit[X] = true;
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            for (int c : conn[curr[0]]) {
+                if (visit[c]) {
+                    continue;
                 }
+                visit[c] = true;
+                dist[c] = curr[1] + 1;
+                queue.offer(new int[]{c, curr[1] + 1});
             }
         }
+//        System.out.println(Arrays.toString(dist));
 //        for (int[] c : conn) {
 //            System.out.println(Arrays.toString(c));
 //        }
         StringBuilder answerSb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            if (conn[X][i] == K) {
-                answerSb.append(i+1).append("\n");
+            if (dist[i] == K) {
+                answerSb.append(i + 1).append("\n");
             }
         }
         System.out.println(answerSb.isEmpty() ? "-1" : answerSb.toString());
