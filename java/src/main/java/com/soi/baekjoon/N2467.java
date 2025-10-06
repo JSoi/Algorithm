3e3c98f6c612eaf0af144a7673390bc90551b890
JSoi <jsoi950715@gmail.com>
2025-10-06 19:19:21 +0900

    BOJ N2467 solved

--- java/src/main/java/com/soi/baekjoon/N2467.java
+++ java/src/main/java/com/soi/baekjoon/N2467.java
@@ -3,7 +3,9 @@ package com.soi.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class N2467 {
     public static void main(String[] args) throws IOException {
@@ -14,19 +16,27 @@ public class N2467 {
         int l = 0;
         int r = 0;
         long[] arr = new long[n];
+        TreeSet<long[]> treeSet = new TreeSet<>(Comparator.comparingLong(a -> a[0]));
         for (int i = 0; i < n; i++) {
             arr[i] = Long.parseLong(tok.nextToken());
+            treeSet.add(new long[]{arr[i], i});
         }
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                long abs = Math.abs(arr[i] + arr[j]);
-                if (abs > min) {
-                    continue;
-                }
+            long num = arr[i];
+            treeSet.remove(new long[]{arr[i], i});
+            long[] ceil = treeSet.ceiling(new long[]{-num, 0});
+            long[] floor = treeSet.floor(new long[]{-num, 0});
+            if (ceil != null && min > Math.abs(ceil[0] + num)) {
                 l = i;
-                r = j;
-                min = abs;
+                r = (int) ceil[1];
+                min = Math.abs(ceil[0] + num);
             }
+            if (floor != null && min > Math.abs(floor[0] + num)) {
+                l = i;
+                r = (int) floor[1];
+                min = Math.abs(floor[0] + num);
+            }
+            treeSet.add(new long[]{arr[i], i});
         }
         System.out.println(arr[l] + " " + arr[r]);
     }
