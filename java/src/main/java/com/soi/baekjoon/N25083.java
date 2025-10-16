34a1acf83b72dcc6841bb57d411a1402d29ef5c7
JSoi <jsoi950715@gmail.com>
2025-10-16 16:38:53 +0900

    BOJ N25083, N27866, N31403 solved

---
+++ java/src/main/java/com/soi/baekjoon/N25083.java
@@ -0,0 +1,16 @@
+package com.soi.baekjoon;
+
+public class N25083 {
+    public static void main(String[] args) {
+        String ans =
+"""
+         ,r'"7
+r`-_   ,'  ,/
+ \\. ". L_r'
+   `~\\/
+      |
+      |
+""";
+        System.out.println(ans);
+    }
+}
