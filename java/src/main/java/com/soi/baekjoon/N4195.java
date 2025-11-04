d3f0981e5dbaf638d8765da47c69874307127376
JSoi <jsoi950715@gmail.com>
2025-11-04 18:17:56 +0900

    BOJ N4195 solved

--- java/src/main/java/com/soi/baekjoon/N4195.java
+++ java/src/main/java/com/soi/baekjoon/N4195.java
@@ -1,10 +1,14 @@
 package com.soi.baekjoon;
 
 import java.io.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class N4195 {
-    private static Map<String, Set<String>> relationMap;
+    private static Map<String, Integer> friendIndexMap;
+    private static int nameCount;
+    private static int[] root;
+    private static int[] size;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,35 +16,44 @@ public class N4195 {
         int testCase = Integer.parseInt(br.readLine());
         for (int i = 0; i < testCase; i++) {
             int relationCount = Integer.parseInt(br.readLine());
-            relationMap = new HashMap<>();
+            nameCount = 0;
+            root = new int[relationCount * 2];
+            size = new int[relationCount * 2];
+            for (int k = 0; k < relationCount * 2; k++) {
+                root[k] = k;
+                size[k] = 1;
+            }
+            friendIndexMap = new HashMap<>();
             for (int r = 0; r < relationCount; r++) {
                 String[] line = br.readLine().split(" ");
-                String a = line[0];
-                String b = line[1];
-                relationMap.computeIfAbsent(a, v -> new HashSet<>()).add(b);
-                relationMap.computeIfAbsent(b, v -> new HashSet<>()).add(a);
-                bw.append(String.valueOf(friendCount(a))).append("\n");
+                String a = line[0], b = line[1];
+                int aIdx = getIdx(a), bIdx = getIdx(b);
+                union(aIdx, bIdx);
+                int root = find(aIdx);
+                bw.append(String.valueOf(size[root])).append("\n");
             }
         }
+        br.close();
         bw.flush();
+        bw.close();
     }
 
-    private static int friendCount(String start) {
-        HashSet<String> friends = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
-        queue.add(start);
-        friends.add(start);
-        // 이미 start,b는 연관되어 있으므로 한 번만 진행
-        while (!queue.isEmpty()) {
-            String recent = queue.poll();
-            for (String f : relationMap.get(recent)) {
-                if (friends.contains(f)) {
-                    continue;
-                }
-                queue.offer(f);
-                friends.add(f);
-            }
+    private static void union(int a, int b) {
+        int aa = find(a), bb = find(b);
+        if (aa == bb) {
+            return;
         }
-        return friends.size();
+        root[bb] = aa;
+        size[aa] += size[bb];
+    }
+
+    private static int getIdx(String name) {
+        friendIndexMap.putIfAbsent(name, nameCount++);
+        return friendIndexMap.get(name);
+    }
+
+    private static int find(int target) {
+        if (root[target] == target) return target;
+        return root[target] = find(root[target]);
     }
 }
