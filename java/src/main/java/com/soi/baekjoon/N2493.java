1e7b453048031232dedb1c5a615becf4b93bbc7b
JSoi <jsoi950715@gmail.com>
2025-11-10 17:47:40 +0900

    BOJ N2493 solved

---
+++ java/src/main/java/com/soi/baekjoon/N2493.java
@@ -0,0 +1,41 @@
+package com.soi.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class N2493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
+        int[] answer = new int[n];
+        Stack<Tower> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(tok.nextToken());
+            while (!stack.isEmpty() && stack.peek().height < input) {
+                stack.pop();
+            }
+            if (!stack.isEmpty() && stack.peek().height >= input) {
+                answer[i] = stack.peek().idx;
+            }
+            stack.push(new Tower(i + 1, input));
+
+        }
+        System.out.println(Arrays.stream(answer).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
+    }
+
+    private static class Tower {
+        int idx;
+        int height;
+
+        public Tower(int idx, int height) {
+            this.idx = idx;
+            this.height = height;
+        }
+    }
+}
