9591a53553e73009a41cba38845060d1b7dab6d9
JSoi <jsoi950715@gmail.com>
2025-11-12 18:46:38 +0900

    BOJ N10159 solved floyd-warshall

--- java/src/main/java/com/soi/baekjoon/N10159.java
+++ java/src/main/java/com/soi/baekjoon/N10159.java
@@ -5,21 +5,18 @@ import java.util.*;
 
 public class N10159 {
     // 18:00 - 18:17
-    private static boolean[][] visit;
-    private static List<Integer>[] conn;
+    private static boolean[][] reachable;
     private static int n;
 
     public static void main(String[] args) throws IOException {
         init();
-        // bfs
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 0; i < n; i++) {
-            bfs(i);
-        }
+        // bfs
+        floydWarshall();
         for (int i = 0; i < n; i++) {
             int count = n;
             for (int j = 0; j < n; j++) {
-                if (visit[i][j] || visit[j][i]) {
+                if (i == j || reachable[i][j] || reachable[j][i]) {
                     count--;
                 }
             }
@@ -32,31 +29,24 @@ public class N10159 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        visit = new boolean[n][n];
-        conn = new List[n];
-        for (int i = 0; i < n; i++) {
-            conn[i] = new ArrayList<>();
-        }
+        reachable = new boolean[n][n];
 
         for (int i = 0; i < m; i++) {
             // a > b
             StringTokenizer tok = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(tok.nextToken()) - 1;
             int b = Integer.parseInt(tok.nextToken()) - 1;
-            conn[a].add(b);
+            reachable[a][b] = true;
         }
     }
 
-    private static void bfs(int start) {
-        visit[start][start] = true;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            for (int c : conn[curr]) {
-                if (!visit[start][c]) {
-                    queue.offer(c);
-                    visit[start][c] = true;
+    private static void floydWarshall() {
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (reachable[i][k] && reachable[k][j]) {
+                        reachable[i][j] = true;
+                    }
                 }
             }
         }
