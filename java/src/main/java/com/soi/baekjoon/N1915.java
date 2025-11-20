ecd3e2fe526ea91c0169bd8430e862b1fa02ca83
JSoi <jsoi950715@gmail.com>
2025-11-20 15:32:29 +0900

    BOJ N1915 solved

---
+++ java/src/main/java/com/soi/baekjoon/N1915.java
@@ -0,0 +1,34 @@
+package com.soi.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N1915 {
+    // 15:22 ~ 15:30
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tok = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(tok.nextToken());
+        int m = Integer.parseInt(tok.nextToken());
+        int[][] map = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = line.charAt(j - 1) == '0' ? 0 : 1;
+                dp[i][j] = map[i][j];
+            }
+        }
+        int maxLen = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (dp[i][j] == 0) continue;
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                maxLen = Math.max(maxLen, dp[i][j]);
+            }
+        }
+        System.out.println(maxLen * maxLen);
+    }
+}
