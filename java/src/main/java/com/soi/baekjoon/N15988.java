298e3b2c6afc2445f36849174b3159a0663a8fc5
JSoi <jsoi950715@gmail.com>
2025-11-24 17:36:48 +0900

    BOJ N15988 solved

---
+++ java/src/main/java/com/soi/baekjoon/N15988.java
@@ -0,0 +1,26 @@
+package com.soi.baekjoon;
+
+import java.io.*;
+
+public class N15988 {
+    private static final int INF = 1_000_000_009;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+        int k = 1000000;
+        long[] dp = new long[k + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= k; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % INF;
+        }
+        while (testCase-- > 0) {
+            int input = Integer.parseInt(br.readLine());
+            bw.append(String.valueOf(dp[input])).append('\n');
+        }
+        bw.flush();
+    }
+}
