bf26e743e1e469b271d2b3638c4d335eac85b7b4
JSoi <jsoi950715@gmail.com>
2025-12-10 17:05:17 +0900

    BOJ N7453 TO (upper_bound, lower_bound)

--- java/src/main/java/com/soi/baekjoon/N7453.java
+++ java/src/main/java/com/soi/baekjoon/N7453.java
@@ -3,8 +3,7 @@ package com.soi.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class N7453 {
@@ -22,21 +21,58 @@ public class N7453 {
             C[i] = Long.parseLong(tok.nextToken());
             D[i] = Long.parseLong(tok.nextToken());
         }
-        Map<Long, Integer> sumMap = new HashMap<>(2 * N);
-        for (int k = 0; k < N; k++) {
-            for (int l = 0; l < N; l++) {
-                long sum = C[k] + D[l];
-                sumMap.put(sum, sumMap.getOrDefault(sum, 0) + 1);
+        long[] AB = new long[N * N]; // AB 합 저장
+        long[] CD = new long[N * N]; // CD 합 저장 (정렬필요)
+
+        int idx = 0;
+        for (long a : A) {
+            for (long b : B) {
+                AB[idx++] = a + b;
             }
         }
 
-        long answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                long sum = A[i] + B[j];
-                answer += sumMap.getOrDefault(-sum, 0);
+        idx = 0;
+        for (long c : C) {
+            for (long d : D) {
+                CD[idx++] = c + d;
             }
         }
+        Arrays.sort(CD);
+
+        long answer = 0;
+        for (long ab : AB) {
+            int upperBound = upperBound(CD, -ab);
+            int lowerBound = lowerBound(CD, -ab);
+            answer += upperBound - lowerBound;
+        }
         System.out.println(answer);
+    }
+
+    static int lowerBound(long[] arr, long target) {
+        int l = 0;
+        int r = arr.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[m] >= target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+
+    static int upperBound(long[] arr, long target) {
+        int l = 0;
+        int r = arr.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[m] > target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
     }
 }
