9eaf4236b9ee1d94df9c9018413ccf19e39327e4
JSoi <jsoi950715@gmail.com>
2025-12-28 04:22:42 +0900

    BOJ N9657 solved

---
+++ java/src/main/java/com/soi/baekjoon/N9657.java
@@ -0,0 +1,15 @@
+package com.soi.baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * <a href = "https://www.acmicpc.net/problem/9657">돌 게임 3</a>
+ */
+public class N9657 {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        // SK : 1,3,4,5,6,8,10,11,12,13,
+        // CY : 2,7,9,14
+        System.out.println(n % 7 == 0 || n % 7 == 2 ? "CY" : "SK");
+    }
+}
