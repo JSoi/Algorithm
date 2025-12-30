70986875295d32d1994918aaa160935b8a3e83e8
JSoi <jsoi950715@gmail.com>
2025-12-30 20:44:30 +0900

    BOJ N10773 solved

---
+++ java/src/main/java/com/soi/baekjoon/N10773.java
@@ -0,0 +1,27 @@
+package com.soi.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class N10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Long> stack = new Stack<>();
+        while (n-- > 0) {
+            long num = Long.parseLong(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        long answer = 0;
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
+        }
+        System.out.println(answer);
+    }
+}
