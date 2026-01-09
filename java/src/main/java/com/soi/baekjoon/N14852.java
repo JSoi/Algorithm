eac01a1da5d3615be7447f52fe9140e44ce7462a
JSoi <jsoi950715@gmail.com>
2026-01-10 00:06:51 +0900

    BOJ N14852 solved

---
+++ java/src/main/java/com/soi/baekjoon/N14852.java
@@ -0,0 +1,23 @@
+package com.soi.baekjoon;
+
+import java.util.Scanner;
+
+public class N14852 {
+    private static final long DIV = 1_000_000_007;
+
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        long[] both = new long[n + 1];
+        long[] upper = new long[n + 1];
+        long[] lower = new long[n + 1];
+        both[0] = upper[0] = lower[0] = 1;
+        upper[1] = lower[1] = 1;
+        both[1] = 2;
+        for (int i = 2; i <= n; i++) {
+            upper[i] = (both[i - 1] + lower[i - 1]) % DIV;
+            lower[i] = (both[i - 1] + upper[i - 1]) % DIV;
+            both[i] = (2 * both[i - 1] + lower[i - 1] + upper[i - 1] + both[i - 2]) % DIV;
+        }
+        System.out.println(both[n]);
+    }
+}
