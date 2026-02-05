f0ca79880a8b53df3835205433107f46990b31f7
JSoi <jsoi950715@gmail.com>
2026-02-05 23:12:28 +0900

    POG L60062 solved

--- java/src/main/java/com/soi/programmers/L60062.java
+++ java/src/main/java/com/soi/programmers/L60062.java
@@ -1,57 +1,64 @@
 package com.soi.programmers;
 
-import java.util.Arrays;
-
 public class L60062 {
     public static void main(String[] args) {
         int solution = new L60062().solution(200, new int[]{0, 10, 50, 80, 120, 160}, new int[]{1, 10, 5, 40, 30});
         System.out.println(solution);
     }
 
-    static int[] friendCleanArr;
-    static int maxLen;
-    static int maxUsedFriendsCount = Integer.MAX_VALUE;
+    static int answer;
+    static int[] extendedWeak;
+    static int nWeak;
+    static int nDist;
 
     public static int solution(int n, int[] weak, int[] dist) {
-        maxLen = n;
-        friendCleanArr = Arrays.stream(dist.clone()).boxed().sorted().mapToInt(Integer::intValue).toArray();
-        fixWeakness(friendCleanArr.length - 1, weak);
-        return maxUsedFriendsCount == Integer.MAX_VALUE ? -1 : maxUsedFriendsCount;
+        nWeak = weak.length;
+        nDist = dist.length;
+        answer = nDist + 1;
+
+        extendedWeak = new int[nWeak * 2];
+        for (int i = 0; i < nWeak; i++) {
+            extendedWeak[i] = weak[i];
+            extendedWeak[i + nWeak] = weak[i] + n;
+        }
+
+        // dist 순열 생성
+        boolean[] used = new boolean[nDist];
+        int[] friendsPerm = new int[nDist];
+        permute(dist, used, friendsPerm, 0);
+
+        return answer > nDist ? -1 : answer;
     }
 
-    static void fixWeakness(int friendIdx, int[] weakStatus) {
-        int count = friendCleanArr.length - friendIdx;
-        if (friendIdx < 0 || count >= maxUsedFriendsCount) {
+    static void permute(int[] dist, boolean[] used, int[] friendsPerm, int depth) {
+        if (depth == nDist) {
+            simulate(friendsPerm);
             return;
         }
-        int friendClean = friendCleanArr[friendIdx];
-        for (int i = 0; i < weakStatus.length; i++) {
-            int start = weakStatus[i];
-            if (start == -1) {
-                continue;
-            }
-            int[] subweakstatus = weakStatus.clone();
-            for (int j = 0; j < subweakstatus.length; j++) {
-                if (i == j || subweakstatus[j] == -1) {
-                    continue;
-                }
-                if ((subweakstatus[i] + friendClean >= subweakstatus[j] && subweakstatus[i] <= subweakstatus[j]) || // 사이
-                        ((subweakstatus[i] + friendClean > maxLen) && (subweakstatus[i] + friendClean) % maxLen >= subweakstatus[j])) {
-                    subweakstatus[j] = -1;
-                }
-            }
-            subweakstatus[i] = -1;
-            if (isAllCovered(subweakstatus)) {
-                maxUsedFriendsCount = Math.min(maxUsedFriendsCount, count);
-            } else {
-                fixWeakness(friendIdx - 1, subweakstatus);
-            }
+        for (int i = 0; i < nDist; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            friendsPerm[depth] = dist[i];
+            permute(dist, used, friendsPerm, depth + 1);
+            used[i] = false;
         }
     }
 
-    static boolean isAllCovered(int[] weak) {
-        return Arrays.stream(weak).filter(w -> w == -1).count() == weak.length;
+    static void simulate(int[] perm) {
+        for (int start = 0; start < nWeak; start++) {
+            int friendIdx = 0;
+            int coverageEnd = extendedWeak[start] + perm[friendIdx];
+            for (int idx = start; idx < start + nWeak; idx++) {
+                if (extendedWeak[idx] > coverageEnd) {
+                    friendIdx++;
+                    if (friendIdx == nDist) {
+                        break;
+                    }
+                    coverageEnd = extendedWeak[idx] + perm[friendIdx];
+                }
+            }
+            answer = Math.min(answer, friendIdx + 1);
+        }
     }
-
 
 }
