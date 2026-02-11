7f2dc524f0f9786ad92960a02382a2dbeff82bca
JSoi <jsoi950715@gmail.com>
2026-02-11 21:33:22 +0900

    [kotlin] BOJ kotlin version

--- kotlin/src/main/kotlin/com/soi/baekjoon/BOJ_2292.kt
+++ kotlin/src/main/kotlin/com/soi/baekjoon/BOJ_2292.kt
@@ -1,12 +1,7 @@
 package com.soi.baekjoon
-
 import java.util.Scanner
 
-class N2292 {
-
-}
-
-private fun getAnswer(userInteger:Long) :Long {
+fun getAnswer(userInteger: Long): Long {
     var lastNumber = 1L;
     var timeCal = 0L;
     while (true) {
@@ -21,5 +16,5 @@ private fun getAnswer(userInteger:Long) :Long {
 
 fun main() {
     val scan = Scanner(System.`in`)
-    println(getAnswer(scan.nextLong()))
+    print(getAnswer(scan.nextLong()))
 }
