920031c9b867e3390022c0c108e33bb632da8217
JSoi <jsoiworks@gmail.com>
2026-03-30 15:32:20 +0900

    [BOJ] 6198 solved - stack

--- java/src/main/java/com/soi/baekjoon/BOJ_6198.java
+++ java/src/main/java/com/soi/baekjoon/BOJ_6198.java
@@ -3,25 +3,22 @@ package com.soi.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class BOJ_6198 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long[] arr = new long[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Long.parseLong(br.readLine());
-        }
+        Deque<Long> stack = new ArrayDeque<>();
         long answer = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            long observable = 0;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] >= arr[i]) {
-                    break;
-                }
-                observable++;
+        for (int i = 0; i < n; i++) {
+            long height = Long.parseLong(br.readLine());
+            while (!stack.isEmpty() && stack.peek() <= height) {
+                stack.pop();
             }
-            answer += observable;
+            answer += stack.size();
+            stack.push(height);
         }
         System.out.println(answer);
     }
