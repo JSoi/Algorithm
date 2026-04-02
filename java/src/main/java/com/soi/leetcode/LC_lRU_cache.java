3a7578acafa1d47c582e596f63e1f0fa199c778c
JSoi <jsoiworks@gmail.com>
2026-04-02 20:53:36 +0900

    [LC] lru_cache solved - list

--- java/src/main/java/com/soi/leetcode/LC_lRU_cache.java
+++ java/src/main/java/com/soi/leetcode/LC_lRU_cache.java
@@ -1,38 +1,73 @@
 package com.soi.leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-
 /**
  * <a href = "https://leetcode.com/problems/lru-cache/description/?envType=problem-list-v2&envId=ssd-ssd1-cache-system-design">Cache System Design</a>
  */
 public class LC_lRU_cache {
-    private final Queue<Integer> getQueue;
-    private final int[] keyVal = new int[10001];
+    private static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final Node[] nodes = new Node[10001];
+    private Node head, tail;
     private final int capacity;
+    private int size;
 
     public LC_lRU_cache(int capacity) {
         this.capacity = capacity;
-        getQueue = new ArrayDeque<>();
-        Arrays.fill(keyVal, -1);
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+        this.size = 0;
     }
 
     public int get(int key) {
-        if (keyVal[key] != -1) {
-            getQueue.remove(key);
-            getQueue.offer(key);
-        }
-        return keyVal[key];
+        // return available node
+        Node node = nodes[key];
+        if (node == null) return -1;
+        remove(node);
+        addToHead(node);
+        return node.value;
     }
 
     public void put(int key, int value) {
-        if (getQueue.size() == capacity && keyVal[key] == -1) {
-            int evictKey = getQueue.poll();
-            keyVal[evictKey] = -1;
+        Node node = nodes[key];
+        if (node == null) {
+            node = new Node(key, value);
+            nodes[key] = node;
+            if (size == capacity) {
+                nodes[tail.prev.key] = null;
+                remove(tail.prev);
+            } else {
+                size++;
+            }
+            addToHead(node);
+        } else {
+            remove(node);
+            addToHead(node);
+            node.value = value;
         }
-        getQueue.remove(key);
-        getQueue.offer(key);
-        keyVal[key] = value;
     }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addToHead(Node node) {
+        head.next.prev = node;
+        node.next = head.next;
+        node.prev = head;
+        head.next = node;
+    }
+
 }
