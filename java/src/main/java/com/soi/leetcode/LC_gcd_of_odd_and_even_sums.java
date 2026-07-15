ce950cc201410a7de70fdc4dfa16987182d63f62
JSoi <jsoiworks@gmail.com>
2026-07-15 21:33:39 +0900

    [LC] gcd_of_odd_and_even_sums solved

---
+++ java/src/main/java/com/soi/leetcode/LC_gcd_of_odd_and_even_sums.java
@@ -0,0 +1,8 @@
+package com.soi.leetcode;
+
+public class LC_gcd_of_odd_and_even_sums {
+    // gcd(n^2, n*(n+1)) = n * gcd(n,n+1) = n * 1 = n
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
