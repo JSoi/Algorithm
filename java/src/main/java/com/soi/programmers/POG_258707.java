dacc4f4246fe09dc7a7cde43bd26f564399e368b
JSoi <jsoiworks@gmail.com>
2026-07-17 01:24:35 +0900

    [POG] 258707 solved

--- java/src/main/java/com/soi/programmers/POG_258707.java
+++ java/src/main/java/com/soi/programmers/POG_258707.java
@@ -1,78 +1,60 @@
 package com.soi.programmers;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.TreeSet;
+import java.util.PriorityQueue;
 
 public class POG_258707 {
-    private static TreeSet<Integer> inHands;
-    private static int targetSum;
 
     public static void main(String[] args) {
-        System.out.println(POG_258707.solution(3, new int[]{1, 2, 3, 4, 5, 8, 6, 7, 9, 10, 11, 12}));
+        POG_258707 pog = new POG_258707();
+        System.out.println(pog.solution(4, new int[]{3, 6, 7, 2, 1, 10, 5, 9, 8, 12, 11, 4})); // 5
+        System.out.println(pog.solution(3, new int[]{1, 2, 3, 4, 5, 8, 6, 7, 9, 10, 11, 12})); // 2
+        System.out.println(pog.solution(2, new int[]{5, 8, 1, 2, 9, 4, 12, 11, 3, 10, 6, 7})); // 4
+        System.out.println(pog.solution(2, new int[]{1, 2, 3, 4, 5, 6})); // 2
+        System.out.println(pog.solution(10, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})); // 1
     }
 
-    public static int solution(int coin, int[] c) {
-        // inital draw
-        inHands = new TreeSet<>();
-        int n = c.length;
-        targetSum = n + 1;
-        Queue<Integer> cards = new LinkedList<>(Arrays.stream(c).boxed().toList());
+    private int sum;
+    private int[] position;
+    private int n;
 
-        for (int d = 0; d < n / 3; d++) {
-            inHands.add(cards.poll());
-        }
+    public int solution(int coin, int[] c) {
         int round = 1;
-        while (true) {
-            // 두 개 draw
-            Integer first = cards.poll();
-            Integer second = cards.poll();
-            int firstDrawMatch = isAbleToDraw(first);
-            if (firstDrawMatch >= 0) {
-                inHands.remove(firstDrawMatch);
+        n = c.length;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        boolean[] used = new boolean[n + 1];
+
+        position = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            position[c[i]] = i;
+        }
+        sum = n + 1;
+        for (int i = 0; i < n / 3; i++) {
+            used[c[i]] = true;
+            if (used[sum - c[i]]) pq.offer(0);
+        }
+
+        for (int i = n / 3; i < n; i++) {
+            used[c[i]] = true;
+            if (used[sum - c[i]]) {
+                if (position[sum - c[i]] < n / 3) {
+                    pq.offer(1);
+                } else {
+                    pq.offer(2);
+                }
             }
-            int secondDrawMatch = isAbleToDraw(second);
-            if (firstDrawMatch >= 0) {
-                inHands.add(firstDrawMatch);
+            if (i % 2 == 0) continue;
+            if (!pq.isEmpty()) {
+                int cost = pq.poll();
+                if (coin - cost < 0) {
+                    return round;
+                }
+                coin -= cost;
+                round++;
+            } else {
+                return round;
             }
-            if (firstDrawMatch > 0 && coin > 0) {
-                inHands.add(first);
-                coin--;
-            }
-            if (secondDrawMatch > 0 && coin > 0) {
-                inHands.add(second);
-                coin--;
-            }
-            if (cards.isEmpty() || !delete()) {
-                break;
-            }
-            round++;
         }
         return round;
-    }
-
-    public static int isAbleToDraw(Integer target) {
-        if (target == null) {
-            return -1;
-        }
-        for (int value : inHands) {
-            if (target + value >= targetSum) {
-                return value;
-            }
-        }
-        return -1;
-    }
-
-    public static boolean delete() {
-        for (int h : inHands) {
-            Integer ceiling = inHands.ceiling(targetSum - h);
-            if (ceiling != null) {
-                inHands.remove(ceiling);
-                inHands.remove(h);
-                return true;
-            }
-        }
-        return false;
     }
 }
